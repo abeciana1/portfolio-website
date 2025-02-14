8b3623f4c0fc715a84102b5ede60500e4a43581d
abeciana1 <alex.beciana@gmail.com>
2025-02-14 16:03:01 -0700

    Enabled auth api key security

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -69,6 +69,9 @@ export interface User {
   id: number;
   updatedAt: string;
   createdAt: string;
+  enableAPIKey?: boolean | null;
+  apiKey?: string | null;
+  apiKeyIndex?: string | null;
   email: string;
   resetPasswordToken?: string | null;
   resetPasswordExpiration?: string | null;
@@ -194,6 +197,9 @@ export interface PayloadMigration {
 export interface UsersSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
+  enableAPIKey?: T;
+  apiKey?: T;
+  apiKeyIndex?: T;
   email?: T;
   resetPasswordToken?: T;
   resetPasswordExpiration?: T;
