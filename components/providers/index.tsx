3fe2dc9dc8edf36001a05826d7a45860a4c8b23a
abeciana1 <alex.beciana@gmail.com>
2025-02-15 09:56:59 -0700

    Added React Query provider and client

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,15 +1,20 @@
 'use client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes'
 import { WrapperI } from '@/definitions/interfaces/general'
+
+const queryClient = new QueryClient();
 
 const CombinedProviders: React.FC<WrapperI> = ({
   children
 }) => {
   return (
     <>
-      <ThemeProvider>
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   )
 }
