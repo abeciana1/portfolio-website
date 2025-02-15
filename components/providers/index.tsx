58e1d38ec2c83d6cb1990b25cfa13a76985ed362
abeciana1 <alex.beciana@gmail.com>
2025-02-15 12:16:15 -0700

    Remove next-themes and 'use client' directive

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,6 +1,4 @@
-'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider } from 'next-themes'
 import { WrapperI } from '@/definitions/interfaces/general'
 
 const queryClient = new QueryClient();
@@ -11,9 +9,7 @@ const CombinedProviders: React.FC<WrapperI> = ({
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        {children}
       </QueryClientProvider>
     </>
   )
