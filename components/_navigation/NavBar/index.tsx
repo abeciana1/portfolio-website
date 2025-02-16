c60ec41a9a2df719e5881d9ed33dfeab0998d6fd
abeciana1 <alex.beciana@gmail.com>
2025-02-16 09:46:18 -0700

    Created fetching for navigation links data

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -1,14 +1,29 @@
-import { use } from 'react'
 import { payload } from '@/src/payload'
+import { use } from 'react';
+import { QueryClient } from '@tanstack/react-query';
+import { NavLink } from '@/src/payload-types'
+
+const fetchNavigationContent = async (queryClient: QueryClient) => {
+  return await queryClient.ensureQueryData({
+    queryKey: ['navigation'],
+    queryFn: () => payload.find({
+      collection: 'navigation-menu',
+      overrideAccess: true
+    }),
+    staleTime: process.env.NODE_ENV === 'production' ? 60 * 1000 * 10 * 3 : 60 * 1000
+  })
+}
 
 const NavBar = () => {
-  const navContentData = use(payload.find({
-    collection: 'navigation-menu',
-    overrideAccess: true,
-  }))
-  console.log('navContentData', navContentData)
+  const queryClient = new QueryClient()
+  const navContentData = use(fetchNavigationContent(queryClient))
+  const {
+    links
+  } = navContentData?.docs[0]
+  console.log('links', links)
   return (
-    <></>
+    <nav>
+    </nav>
   )
 }
 
