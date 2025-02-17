02636baf971de09c538e496f822e40e64903ee6f
abeciana1 <alex.beciana@gmail.com>
2025-02-17 14:12:00 -0700

    Added s3 media link hostname

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ab-ph-us-west.s3.us-west-1.amazonaws.com'
+      }
+    ]
+  }
 }
 
 export default withPayload(nextConfig)
