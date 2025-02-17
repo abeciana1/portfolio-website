b49ef939c84728e296573f62b303f81226299bc4
abeciana1 <alex.beciana@gmail.com>
2025-02-16 23:22:30 -0700

    Updated imports

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WrapperT } from '@/types/general'
+import { WrapperI } from '@/types/general'
 
 const queryClient = new QueryClient();
 
-const CombinedProviders: React.FC<WrapperT> = ({
+const CombinedProviders: React.FC<WrapperI> = ({
   children
 }) => {
   return (
--- types/general.d.ts
+++ types/general.d.ts
@@ -1,3 +1,3 @@
-export type WrapperT = {
+export interface WrapperI {
   children: React.ReactNode;
 }
