142f5496a0483ad29a1387e4dbf7aa27b73736a2
abeciana1 <alex.beciana@gmail.com>
2025-02-17 18:40:20 -0700

    Added test

---
+++ components/_navigation/SocialLink/__test__/index.test.tsx
@@ -0,0 +1,21 @@
+import SocialLink from '../'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+describe('SocialLink', () => {
+  beforeEach(() => {
+    render(
+      <SocialLink
+        link='https://www.behance.net/alexbeciana'
+        label='Behance'
+      />
+    )
+  })
+  test('should render', () => {
+    const link = screen.getByRole('link', {
+      name: 'Behance'
+    })
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', 'https://www.behance.net/alexbeciana')
+  })
+})
