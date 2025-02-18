5fab3a2c41a31ee99e1ff48cfe967847d2c92e01
abeciana1 <alex.beciana@gmail.com>
2025-02-17 17:49:33 -0700

    Mapped links data

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -1,8 +1,11 @@
 import { payload } from '@/src/payload'
 import { use } from 'react';
 import { QueryClient } from '@tanstack/react-query';
-// import { NavLink } from '@/src/payload-types'
-import { NavLogo } from '@/components/_navigation'
+import { NavLink as NavLinkI } from '@/src/payload-types'
+import {
+  NavLogo,
+  NavLink
+} from '@/components/_navigation'
 import { NavBarContentI } from '@/types/navigation'
 
 const fetchNavigationContent = async (queryClient: QueryClient) => {
@@ -15,6 +18,8 @@ const fetchNavigationContent = async (queryClient: QueryClient) => {
     staleTime: process.env.NODE_ENV === 'production' ? 60 * 1000 * 10 * 3 : 60 * 1000
   })
 }
+
+type NavLinkType = Pick<NavLinkI, 'id' | 'link' | 'label'>
 
 const NavBar = () => {
   const queryClient = new QueryClient()
@@ -33,13 +38,33 @@ const NavBar = () => {
     height
   } = logo
   return (
-    <nav className='relative px-5 sm:px-10 py-4'>
+    <nav className='relative px-5 sm:px-10 py-4 flex items-center justify-between'>
       <NavLogo
         src={webpUrl}
         alt={alt}
         width={width}
         height={height}
       />
+      {links && (links?.length > 0) &&
+        <ul
+          className='flex items-center gap-16'
+          data-test-id='navLinksList'
+        >
+          {links?.map((link: NavLinkType, index) => {
+            return (
+              <NavLink
+                key={index}
+                link={link.link as string}
+                label={link.label as string}
+              />
+            )
+          })}
+        </ul>
+      }
+      {socialLinks && (socialLinks?.length > 0) &&
+        <ul data-test-id='socialLinksList'>
+        </ul>
+      }
     </nav>
   )
 }
