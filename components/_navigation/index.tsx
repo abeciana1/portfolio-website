c855e21e3b3cb046c53ae982c93af32f259b0c50
abeciana1 <alex.beciana@gmail.com>
2025-02-17 18:30:34 -0700

    Exported SocialLink

--- components/_navigation/index.tsx
+++ components/_navigation/index.tsx
@@ -1,9 +1,11 @@
 import NavBar from "@/components/_navigation/NavBar";
 import NavLogo from '@/components/_navigation/NavLogo'
 import NavLink from '@/components/_navigation/NavLink'
+import SocialLink from '@/components/_navigation/SocialLink'
 
 export {
   NavBar,
   NavLogo,
-  NavLink
+  NavLink,
+  SocialLink
 }
