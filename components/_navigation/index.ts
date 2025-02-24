770bc93eeb44db8650472f50967184cded9fc1bf
abeciana1 <alex.beciana@gmail.com>
2025-02-23 17:02:45 -0700

    Added MobileMenu component

--- components/_navigation/index.ts
+++ components/_navigation/index.ts
@@ -2,10 +2,12 @@ import NavBar from "@/components/_navigation/NavBar";
 import NavLogo from '@/components/_navigation/NavLogo'
 import NavLink from '@/components/_navigation/NavLink'
 import SocialLink from '@/components/_navigation/SocialLink'
+import MobileMenu from '@/components/_navigation/MobileMenu'
 
 export {
   NavBar,
   NavLogo,
   NavLink,
-  SocialLink
+  SocialLink,
+  MobileMenu
 }
