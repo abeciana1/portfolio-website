397df3cc3650dd9f941b2788dafe5a401017fc01
abeciana1 <alex.beciana@gmail.com>
2025-02-23 17:03:49 -0700

    Removed unused imports

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,7 @@
 import { headers as getHeaders } from 'next/headers.js'
 import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
+import { payload } from '@/src/payload'
 
 // import config from '@/payload.config'
 import config from '@/src/payload.config'
@@ -13,7 +13,6 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
     <div>
