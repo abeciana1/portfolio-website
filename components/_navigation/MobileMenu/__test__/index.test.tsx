b9fda7127f7bb25731943ad71721d0f59e556999
abeciana1 <alex.beciana@gmail.com>
2025-03-02 17:22:47 -0700

    Added tests

--- components/_navigation/MobileMenu/__test__/index.test.tsx
+++ components/_navigation/MobileMenu/__test__/index.test.tsx
@@ -1,9 +1,29 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import MobileMenu from '..'
+import userEvent from '@testing-library/user-event'
 
-// describe('MobileMenu', () => {
-//   beforeEach(() => {
-    
-//   })
-// })
+describe('MobileMenu', () => {
+  beforeEach(() => {
+    render(
+      <MobileMenu
+        links={[{ id: 1, link: '/about', label: 'About' }]}
+        socialLinks={[{ id: 2, link: 'Behance', label: 'Behance' }]}
+      />,
+    )
+  })
+  test('should render hamburger menu button', () => {
+    const menuBtn = screen.getByTestId('mobile-btn')
+    expect(menuBtn).toBeInTheDocument()
+  })
+  test('should render navigation links on button click', async () => {
+    const menuBtn = screen.getByRole('button')
+    await userEvent.click(menuBtn)
+    const navLinksList = screen.getByTestId('navLinksList')
+    const socialLinksList = screen.getByTestId('socialLinksList')
+    expect(navLinksList).toBeInTheDocument()
+    expect(socialLinksList).toBeInTheDocument()
+    expect(navLinksList.children.length).toBe(1)
+    expect(socialLinksList.children.length).toBe(1)
+  })
+})
