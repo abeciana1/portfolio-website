8d05e6e393f81e6a72ac7d6ffc47375ca0e448c5
abeciana1 <alex.beciana@gmail.com>
2025-03-05 19:04:14 -0700

    Added section id, border and arrow rendering

--- src/blocks/hero-section.ts
+++ src/blocks/hero-section.ts
@@ -12,6 +12,12 @@ export const HeroSection: Block = {
       },
       fields: [
         {
+          name: 'sectionId',
+          label: 'Section ID',
+          type: 'text',
+          required: true,
+        },
+        {
           name: 'title',
           label: 'Title',
           type: 'text',
@@ -42,7 +48,7 @@ export const HeroSection: Block = {
             name: 'ctaPrimary',
             label: 'Primary CTA (Optional)',
           },
-        }),
+        })
       ],
     },
     {
@@ -59,6 +65,43 @@ export const HeroSection: Block = {
             label: 'Secondary CTA (Optional)',
           },
         }),
+        {
+          name: 'border',
+          label: 'Use border',
+          type: 'checkbox',
+          defaultValue: false,
+        },
+        {
+          name: 'arrow',
+          label: 'Use arrow',
+          type: 'checkbox',
+          defaultValue: false,
+        },
+        {
+          name: 'arrowDirection',
+          label: 'Arrow direction',
+          type: 'select',
+          hasMany: false,
+          options: [
+            {
+              label: 'Right',
+              value: 'right',
+            },
+            {
+              label: 'Down',
+              value: 'down'
+            }
+          ],
+          admin: {
+            condition: (data) => {
+              if (data?.arrow) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          }
+        }
       ],
     },
     {
@@ -68,5 +111,14 @@ export const HeroSection: Block = {
       relationTo: 'media',
       required: true,
     },
+    {
+      name: 'imageGradient',
+      label: 'Add gradient to image',
+      type: 'checkbox',
+      admin: {
+        description: 'Add a gradient aura to the image'
+      },
+      defaultValue: false,
+    }
   ]
 }
