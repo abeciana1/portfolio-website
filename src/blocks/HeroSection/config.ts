a0d453652c75a102bceeb9fe672161e0667d372c
abeciana1 <alex.beciana@gmail.com>
2025-03-06 20:56:08 -0700

    Added nested CTA blocks

--- src/blocks/HeroSection/config.ts
+++ src/blocks/HeroSection/config.ts
@@ -1,5 +1,5 @@
 import { type Block } from 'payload';
-import { link } from '@/src/fields/link'
+import { CallToAction } from '@/src/blocks/CallToAction/config'
 
 export const HeroSection: Block = {
   slug: 'hero-section',
@@ -36,73 +36,13 @@ export const HeroSection: Block = {
       ]
     },
     {
-      type: 'collapsible',
-      label: 'Primary CTA (Optional)',
-      admin: {
-        initCollapsed: true,
-      },
-      fields: [
-        link({
-          required: false,
-          overrides: {
-            name: 'ctaPrimary',
-            label: 'Primary CTA (Optional)',
-          },
-        })
+      name: 'callToAction',
+      label: 'Call-to-Action',
+      type: 'blocks',
+      blocks: [
+        CallToAction
       ],
-    },
-    {
-      type: 'collapsible',
-      label: 'Secondary CTA (Optional)',
-      admin: {
-        initCollapsed: true,
-      },
-      fields: [
-        link({
-          required: false,
-          overrides: {
-            name: 'ctaSecondary',
-            label: 'Secondary CTA (Optional)',
-          },
-        }),
-        {
-          name: 'border',
-          label: 'Use border',
-          type: 'checkbox',
-          defaultValue: false,
-        },
-        {
-          name: 'arrow',
-          label: 'Use arrow',
-          type: 'checkbox',
-          defaultValue: false,
-        },
-        {
-          name: 'arrowDirection',
-          label: 'Arrow direction',
-          type: 'select',
-          hasMany: false,
-          options: [
-            {
-              label: 'Right',
-              value: 'right',
-            },
-            {
-              label: 'Down',
-              value: 'down'
-            }
-          ],
-          admin: {
-            condition: (data) => {
-              if (data?.arrow) {
-                return true;
-              } else {
-                return false;
-              }
-            }
-          }
-        }
-      ],
+      required: false,
     },
     {
       name: 'image',
