35cea7a26234c141efcec5a3350e17a89d2f4b2e
abeciana1 <alex.beciana@gmail.com>
2025-03-08 09:46:46 -0700

    Added content without style

--- src/blocks/HeroSection/component.tsx
+++ src/blocks/HeroSection/component.tsx
@@ -1,8 +1,24 @@
 import { HeroSectionProps } from '@/types/blockTypes'
 
-const HeroSection: React.FC<HeroSectionProps> = () => {
+const HeroSection: React.FC<HeroSectionProps> = ({
+  sectionId,
+  title,
+  subtitle,
+  description,
+  image,
+  disableInnerContainer = false
+}) => {
+  // console.log('image', image)
   return (
-    <></>
+    <section aria-label='section' id={sectionId}>
+      <div>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <div>{description}</div>
+      </div>
+      <div>
+      </div>
+    </section>
   )
 }
 
