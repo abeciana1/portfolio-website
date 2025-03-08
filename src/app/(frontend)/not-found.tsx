ae4de24aae5d262739a6554d8949581ccda1f656
abeciana1 <alex.beciana@gmail.com>
2025-03-07 20:12:48 -0700

    Created 404 page

---
+++ src/app/(frontend)/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="prose max-w-none">
+      <h1 style={{ marginBottom: 0 }}>404</h1>
+      <p className="mb-4">This page could not be found.</p>
+    </div>
+  )
+}
+
+export default NotFound
