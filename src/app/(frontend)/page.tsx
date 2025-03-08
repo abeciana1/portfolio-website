10fe189d3be017769bb0ab1a2d8f44348cce08e7
abeciana1 <alex.beciana@gmail.com>
2025-03-07 22:22:04 -0700

    Adjusted page entry point file

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,21 +1,5 @@
-import { headers as getHeaders } from 'next/headers.js'
-import { getPayload } from 'payload'
-import React from 'react'
-import { payload } from '@/src/payload'
+import PageTemplate from './[slug]/page'
 
-// import config from '@/payload.config'
-import config from '@/src/payload.config'
-import './styles.css'
+export default PageTemplate
 
-export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-
-  return (
-    <div>
-    </div>
-  )
-}
+// export { generateMetadata }
