b75404b072fd32111ecac12c032acd659e29429a
abeciana1 <alex.beciana@gmail.com>
2025-03-08 09:45:55 -0700

    Wrote tests

---
+++ src/blocks/HeroSection/__test__/index.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import HeroSection from '@/src/blocks/HeroSection/component'
+
+describe('HeroSection', () => {
+  beforeEach(() => {
+    render(
+      <HeroSection
+        blockType="hero-section"
+        sectionId="hero-section"
+        title="Welcome to My Website"
+        subtitle="This is a sample website"
+        description="This is a description of my website"
+        image={{
+          id: 1,
+          alt: 'Hero Image',
+          convertWebp: true,
+          url: 'http://www.example.com/image.jpg',
+          updatedAt: '2025-01-23',
+          createdAt: '2025-01-23',
+          height: 100,
+          width: 100,
+        }}
+      />,
+    )
+  })
+  test('should render a section id', () => {
+    const section = screen.getByLabelText('section')
+    expect(section).toHaveAttribute('id', 'hero-section')
+  })
+  test('should render a title', () => {
+    const title = screen.getByText('Welcome to My Website')
+    expect(title).toBeInTheDocument()
+  })
+  test('should render an image', () => {
+    const img = screen.getByRole('img')
+    expect(img).toBeInTheDocument()
+    expect(img).toHaveAttribute('alt', 'Hero Image')
+    expect(img).toHaveAttribute('height', 100)
+    expect(img).toHaveAttribute('width', 100)
+  })
+})
