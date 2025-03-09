6cda9e9eb9089b5b8472e8f8899a4e07fce7117b
abeciana1 <alex.beciana@gmail.com>
2025-03-08 19:29:28 -0700

    Created gradient variant components

---
+++ gradients/Variant2.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+const Variant2 = () => (
+  <svg
+    width={782}
+    height={680}
+    viewBox="0 0 782 680"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_f_13_2812)">
+      <ellipse
+        cx={391}
+        cy={417.2}
+        rx={190.4}
+        ry={178.8}
+        fill="url(#paint0_linear_13_2812)"
+      />
+    </g>
+    <g filter="url(#filter1_f_13_2812)">
+      <ellipse
+        cx={391}
+        cy={308.8}
+        rx={112.8}
+        ry={108.8}
+        fill="url(#paint1_linear_13_2812)"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_f_13_2812"
+        x={0.599976}
+        y={38.3999}
+        width={780.8}
+        height={757.6}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur
+          stdDeviation={100}
+          result="effect1_foregroundBlur_13_2812"
+        />
+      </filter>
+      <filter
+        id="filter1_f_13_2812"
+        x={78.2}
+        y={0}
+        width={625.6}
+        height={617.6}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur
+          stdDeviation={100}
+          result="effect1_foregroundBlur_13_2812"
+        />
+      </filter>
+      <linearGradient
+        id="paint0_linear_13_2812"
+        x1={391}
+        y1={238.4}
+        x2={391}
+        y2={596}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#00C2FF" stopOpacity={0.46} />
+        <stop offset={1} stopColor="#FF008A" stopOpacity={0.28} />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_13_2812"
+        x1={391}
+        y1={200}
+        x2={391}
+        y2={417.6}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#00C2FF" stopOpacity={0.44} />
+        <stop offset={1} stopColor="#B40463" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default Variant2;
---
+++ gradients/Variant3.tsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+const Variant3 = () => (
+  <svg
+    width={718}
+    height={660}
+    viewBox="0 0 718 660"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_f_13_2813)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M361.921 316.801C411.237 317.082 439.973 367.996 469.047 407.775C495.865 444.469 522.636 483.309 516.518 528.323C509.734 578.233 481.501 624.987 436.445 647.623C390.553 670.679 335.951 665.16 290.064 642.094C245.212 619.549 209.413 579.156 201.671 529.612C194.448 483.385 225.098 443.943 253.256 406.549C282.978 367.078 312.469 316.52 361.921 316.801Z"
+        fill="url(#paint0_linear_13_2813)"
+      />
+    </g>
+    <g filter="url(#filter1_f_13_2813)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M360.962 200.801C394.089 200.989 413.391 235.167 432.92 261.871C450.935 286.504 468.917 312.577 464.807 342.795C460.251 376.299 441.286 407.685 411.021 422.88C380.195 438.358 343.518 434.653 312.694 419.169C282.567 404.034 258.52 376.919 253.32 343.66C248.467 312.628 269.056 286.15 287.97 261.048C307.935 234.552 327.744 200.612 360.962 200.801Z"
+        fill="url(#paint1_linear_13_2813)"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_f_13_2813"
+        x={0.599976}
+        y={116.8}
+        width={716.8}
+        height={745.6}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur
+          stdDeviation={100}
+          result="effect1_foregroundBlur_13_2813"
+        />
+      </filter>
+      <filter
+        id="filter1_f_13_2813"
+        x={52.6}
+        y={0.800049}
+        width={612.8}
+        height={632}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feGaussianBlur
+          stdDeviation={100}
+          result="effect1_foregroundBlur_13_2813"
+        />
+      </filter>
+      <linearGradient
+        id="paint0_linear_13_2813"
+        x1={517.4}
+        y1={637.6}
+        x2={517.4}
+        y2={418.4}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#85357E" stopOpacity={0} />
+        <stop offset={1} stopColor="#FA0D3F" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_13_2813"
+        x1={252.6}
+        y1={408}
+        x2={259.528}
+        y2={138.245}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#FB0A3F" />
+        <stop offset={0.9492} stopColor="#F9A641" stopOpacity={0.55} />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default Variant3;
