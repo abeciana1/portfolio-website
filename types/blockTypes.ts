00b53f1c730fd598f743c5ef17790209544ae54d
abeciana1 <alex.beciana@gmail.com>
2025-03-08 22:18:24 -0700

    Created ImageBlockProps and adjusted media [] prop

--- types/blockTypes.ts
+++ types/blockTypes.ts
@@ -2,11 +2,22 @@ import {
   type HeroSection,
   type SkillBlock
 } from '@/src/payload-types'
+import { ImageT } from '@/types/general'
 
 type InnerContainer = {
   disableInnerContainer?: boolean;
 }
 
-export type HeroSectionProps = HeroSection & InnerContainer
+export type HeroSectionProps = HeroSection & InnerContainer & {
+  media: ImageBlockProps[]
+}
 
 export type SkillProps = SkillBlock & InnerContainer
+
+export type ImageBlockProps = {
+  image: ImageT;
+  gradient?: boolean;
+  gradientXFlip?: boolean;
+  gradientYFlip?: boolean;
+  gradientSelect?: 'Variant1' | 'Variant2' | 'Variant3' | 'Variant4';
+}
