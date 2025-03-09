cc9dd36800a3fbfad0c09004a2a9564a755600da
abeciana1 <alex.beciana@gmail.com>
2025-03-08 17:25:56 -0700

    Removed trailing comma

--- src/collections/pages.ts
+++ src/collections/pages.ts
@@ -37,7 +37,7 @@ const Page: CollectionConfig = {
             {
               name: 'slug',
               type: 'text',
-              label: 'Slug',
+              label: 'Slug'
             },
             {
               name: 'layout',
