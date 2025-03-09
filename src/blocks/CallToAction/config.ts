ca9069e55002c6c6cb1785a808883cb2068e7693
abeciana1 <alex.beciana@gmail.com>
2025-03-08 17:25:14 -0700

    Adjusted conditional code

--- src/blocks/CallToAction/config.ts
+++ src/blocks/CallToAction/config.ts
@@ -5,71 +5,47 @@ export const CallToAction: Block = {
   slug: 'call-to-action',
   fields: [
     {
-      type: 'collapsible',
-      label: 'Call-To-Action link',
-      admin: {
-        initCollapsed: true,
-      },
-      fields: [
-        {
-          name: 'style',
-          label: 'Primary or secondary CTA',
-          type: 'select',
-          options: [
-            {label: 'Primary', value: 'primary'},
-            {label: 'Secondary', value: 'secondary'}
-          ]
-        },
-        link({
-          required: false,
-        }),
-        {
-          name: 'border',
-          label: 'Use border',
-          type: 'checkbox',
-          defaultValue: false,
-          admin: {
-            condition: (data) => {
-              if (data?.style === 'secondary') {
-                return true
-              } else {
-                return false
-              }
-            }
-          }
-        },
-        {
-          name: 'arrow',
-          label: 'Use arrow',
-          type: 'checkbox',
-          defaultValue: false,
-        },
-        {
-          name: 'arrowDirection',
-          label: 'Arrow direction',
-          type: 'select',
-          hasMany: false,
-          options: [
-            {
-              label: 'Right',
-              value: 'right',
-            },
-            {
-              label: 'Down',
-              value: 'down'
-            }
-          ],
-          admin: {
-            condition: (data) => {
-              if (data?.arrow) {
-                return true;
-              } else {
-                return false;
-              }
-            }
-          }
-        }
+      name: 'style',
+      label: 'Primary or secondary CTA',
+      type: 'select',
+      options: [
+        {label: 'Primary', value: 'primary'},
+        {label: 'Secondary', value: 'secondary'}
       ],
+      hooks: {
+        beforeChange: [
+          async ({ blockData }) => {
+            if (blockData && blockData.link.label) {
+              blockData.blockName = blockData.link.label
+            }
+          }
+        ],
+      },
     },
-  ]
+    link({
+      required: false,
+    }),
+    {
+      name: 'arrow',
+      label: 'Use arrow',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'arrowDirection',
+      label: 'Arrow direction',
+      type: 'select',
+      hasMany: false,
+      options: [
+        {
+          label: 'Right',
+          value: 'right',
+        },
+        {
+          label: 'Down',
+          value: 'down'
+        }
+      ]
+    }
+  ],
 }
