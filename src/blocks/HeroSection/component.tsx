49f8a0e382c10bd55d13da72a46b3ee90da4381e
abeciana1 <alex.beciana@gmail.com>
2025-03-11 21:14:32 -0600

    Adjusted breakpoints and replaced innerContainer styles

--- src/blocks/HeroSection/component.tsx
+++ src/blocks/HeroSection/component.tsx
@@ -25,11 +25,11 @@ const HeroSection: React.FC<HeroSectionProps> = ({
     <section
       aria-label='section'
       id={sectionId}
-      className={clsx('relative flex flex-col md:flex-row gap-40 items-center mx-auto max-w-fit', {
-        ['px-20 py-12']: enableInnerContainer
+      className={clsx('relative flex flex-col md:flex-row gap-5 lg:gap-10 xl:gap-40 items-center mx-auto max-w-fit', {
+        [innerContainer]: enableInnerContainer
       })}
     >
-      <div className='max-w-[560px]'>
+      <div className='lg:max-w-[500px] md:max-w-[400px]'>
         <Heading1 text={title as string} />
         <Heading2 text={subtitle as string} />
         <div className='leading-loose text-xl font-semibold'>{description}</div>
