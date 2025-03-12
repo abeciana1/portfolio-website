c72236fff00759c79cab83a38ba0652bbf4a25be
abeciana1 <alex.beciana@gmail.com>
2025-03-11 20:28:27 -0600

    Changed fields

--- src/blocks/Image/config.ts
+++ src/blocks/Image/config.ts
@@ -5,51 +5,58 @@ export const Image: Block = {
   interfaceName: 'ImageBlock',
   fields: [
     {
-      type: 'collapsible',
+      name: 'image',
       label: 'Image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'gradient',
+      label: 'Add gradient to image',
+      type: 'checkbox',
       admin: {
-        initCollapsed: true,
+        description: 'Add a gradient aura to the image'
       },
-      fields: [
-        {
-          name: 'image',
-          label: 'Image',
-          type: 'upload',
-          relationTo: 'media',
-          required: true,
-        },
-        {
-          name: 'gradient',
-          label: 'Add gradient to image',
-          type: 'checkbox',
-          admin: {
-            description: 'Add a gradient aura to the image'
-          },
-          defaultValue: false,
-        },
-        {
-          name: 'gradientXFlip',
-          label: 'Flip gradient along X-axis',
-          type: 'checkbox',
-          defaultValue: false
-        },
-        {
-          name: 'gradientYFlip',
-          label: 'Flip gradient along X-axis',
-          type: 'checkbox',
-          defaultValue: false
-        },
-        {
-          name: 'gradientSelect',
-          label: 'Gradient variant select',
-          type: 'select',
-          options: [
-            { label: 'Variant 1', value: 'Variant1' },
-            { label: 'Variant 2', value: 'Variant2' },
-            { label: 'Variant 3', value: 'Variant3' },
-            { label: 'Variant 4', value: 'Variant4' }
-          ]
-        }
+      defaultValue: false,
+    },
+    {
+      name: 'gradientXFlip',
+      label: 'Flip gradient along X-axis',
+      type: 'checkbox',
+      defaultValue: false
+    },
+    {
+      name: 'gradientYFlip',
+      label: 'Flip gradient along X-axis',
+      type: 'checkbox',
+      defaultValue: false
+    },
+    {
+      name: 'forcedWidth',
+      type: 'number',
+      label: 'Force image width (in pixels)',
+      admin: {
+        description: 'Set a specific width for the image, overriding the aspect ratio'
+      }
+    },
+    {
+      name: 'forcedHeight',
+      type: 'number',
+      label: 'Force image height (in pixels)',
+      admin: {
+        description: 'Set a specific height for the image, overriding the aspect ratio'
+      }
+    },
+    {
+      name: 'gradientSelect',
+      label: 'Gradient variant select',
+      type: 'select',
+      options: [
+        { label: 'Variant 1', value: 'Variant1' },
+        { label: 'Variant 2', value: 'Variant2' },
+        { label: 'Variant 3', value: 'Variant3' },
+        { label: 'Variant 4', value: 'Variant4' }
       ]
     }
   ]
