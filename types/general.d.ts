730cee152bdefbca86c543a0e9960372e0953bb3
abeciana1 <alex.beciana@gmail.com>
2025-03-11 21:13:48 -0600

    Created NavLogoT type

--- types/general.d.ts
+++ types/general.d.ts
@@ -4,6 +4,13 @@ export interface WrapperI {
   children: React.ReactNode;
 }
 
+export type NavLogoT = {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}
+
 export type ImageT = CMSMediaT & {
   alt: string;
   width: number;
@@ -22,4 +29,6 @@ export type GradientProps = {
   variant?: 'Variant1' | 'Variant2' | 'Variant3' | 'Variant4';
   gradientXFlip?: boolean;
   gradientYFlip?: boolean;
+  height: number;
+  width: number;
 }
