af57361f38e1b174a74f9b81e8d6ec7c7fbc9e16
abeciana1 <alex.beciana@gmail.com>
2025-03-13 20:46:27 -0600

    Adjusted centered positioning

--- components/_navigation/Toolbar/index.tsx
+++ components/_navigation/Toolbar/index.tsx
@@ -38,74 +38,76 @@ const Toolbar = () => {
 
   return (
     <MotionConfig transition={transition}>
-      <div className="z-50 fixed bottom-8 left-[50%] mx-auto" ref={ref}>
-        <div className="h-full w-full max-w-fit rounded-xl border border-zinc-950/10 bg-white">
-          <div className="overflow-hidden">
-            <AnimatePresence initial={false} mode="sync">
-              {isOpen && (
-                <motion.div
-                  key="content"
-                  initial={{ height: 0 }}
-                  animate={{ height: heightContent || 0 }}
-                  exit={{ height: 0 }}
-                  style={{ width: maxWidth }}
-                >
-                  <div ref={contentRef} className="p-2">
-                    {ITEMS.map((item) => {
-                      if (!item.content) return null;
-                      const isSelected = active === item.id;
-                      return (
-                        <motion.div
-                          key={item.id}
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: isSelected ? 1 : 0 }}
-                          exit={{ opacity: 0 }}
-                        >
-                          <div className={cx('px-2 pt-2 text-sm', { block: isSelected, hidden: !isSelected })}>
-                            {item.content}
-                          </div>
-                        </motion.div>
-                      );
-                    })}
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
-          <div className="flex space-x-2 p-2" ref={menuRef}>
-            {ITEMS.map((item) => {
-              if (item.content) {
-                return (
-                  <button
-                    key={item.id}
-                    aria-label={item.label}
-                    className={cx(
-                      'relative flex h-9 w-9 shrink-0 select-none appearance-none items-center justify-center rounded-lg text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98]',
-                      { 'bg-zinc-100 text-zinc-800': active === item.id }
-                    )}
-                    type="button"
-                    onClick={() => {
-                      if (!item.content) return;
-                      if (!isOpen) setIsOpen(true);
-                      if (active === item.id) {
-                        setIsOpen(false);
-                        setActive(null);
-                        return;
-                      }
-                      setActive(item.id);
-                      }}
+      <div className="z-50 fixed w-full" ref={ref}>
+        <div className='w-full relative flex justify-center'>
+          <div className="h-full w-full max-w-fit rounded-xl border border-zinc-950/10 bg-white">
+            <div className="overflow-hidden">
+              <AnimatePresence initial={false} mode="sync">
+                {isOpen && (
+                  <motion.div
+                    key="content"
+                    initial={{ height: 0 }}
+                    animate={{ height: heightContent || 0 }}
+                    exit={{ height: 0 }}
+                    style={{ width: maxWidth }}
                   >
-                    {item.component}
-                  </button>
-                )
-              } else {
-                return (
-                  <div key={item?.id}>
-                    { item.component }
-                  </div>
-                )
-              }
-            })}
+                    <div ref={contentRef} className="p-2">
+                      {ITEMS.map((item) => {
+                        if (!item.content) return null;
+                        const isSelected = active === item.id;
+                        return (
+                          <motion.div
+                            key={item.id}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: isSelected ? 1 : 0 }}
+                            exit={{ opacity: 0 }}
+                          >
+                            <div className={cx('px-2 pt-2 text-sm', { block: isSelected, hidden: !isSelected })}>
+                              {item.content}
+                            </div>
+                          </motion.div>
+                        );
+                      })}
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+            <div className="flex space-x-2 p-2" ref={menuRef}>
+              {ITEMS.map((item) => {
+                if (item.content) {
+                  return (
+                    <button
+                      key={item.id}
+                      aria-label={item.label}
+                      className={cx(
+                        'relative flex h-9 w-9 shrink-0 select-none appearance-none items-center justify-center rounded-lg text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98]',
+                        { 'bg-zinc-100 text-zinc-800': active === item.id }
+                      )}
+                      type="button"
+                      onClick={() => {
+                        if (!item.content) return;
+                        if (!isOpen) setIsOpen(true);
+                        if (active === item.id) {
+                          setIsOpen(false);
+                          setActive(null);
+                          return;
+                        }
+                        setActive(item.id);
+                        }}
+                    >
+                      {item.component}
+                    </button>
+                  )
+                } else {
+                  return (
+                    <div key={item?.id}>
+                      { item.component }
+                    </div>
+                  )
+                }
+              })}
+            </div>
           </div>
         </div>
       </div>
