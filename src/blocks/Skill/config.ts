52c26d9cfeb6aa00bf9dd6aebb3145bf441af3a4
abeciana1 <alex.beciana@gmail.com>
2025-03-13 21:07:44 -0600

    Removed collapsible field

--- src/blocks/Skill/config.ts
+++ src/blocks/Skill/config.ts
@@ -4,26 +4,17 @@ export const Skill: Block = {
   slug: 'skill-block',
   fields: [
     {
-      type: 'collapsible',
-      label: ({ data }: {data : { title: string}}) => `Skill: ${data?.title}` || 'Skill: Untitled',
-      admin: {
-        initCollapsed: true,
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-          label: 'Title',
-          required: true,
-        },
-        {
-          name: 'skillIcon',
-          label: 'Skill Icon',
-          type: 'upload',
-          relationTo:'media',
-          required: true,
-        }
-      ]
+      name: 'title',
+      type: 'text',
+      label: 'Title',
+      required: true,
+    },
+    {
+      name: 'skillIcon',
+      label: 'Skill Icon',
+      type: 'upload',
+      relationTo:'media',
+      required: true,
     }
   ]
 }
