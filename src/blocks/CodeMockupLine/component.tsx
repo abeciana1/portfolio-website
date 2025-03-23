82b7808dd83b0f9784a4733d1548922424c9c39a
abeciana1 <alex.beciana@gmail.com>
2025-03-23 08:57:32 -0600

    Created CodeMockupLine

---
+++ src/blocks/CodeMockupLine/component.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx'
+import { type CodeMockupLineProps } from '@/types/blockTypes'
+
+const CodeMockupLine: React.FC<CodeMockupLineProps> = ({
+  text,
+  prefix,
+  textColor,
+}) => {
+  return (
+      <pre data-prefix={prefix} className={clsx("flex", {
+          ["text-black"]: textColor === "black",
+          ["text-white"]: textColor === "white",
+          ["text-green-400"]: textColor === "success",
+          ["text-[#F7B538]"]: textColor === "warning"
+      })}>
+          <code className="whitespace-normal">
+              {text}
+          </code>
+      </pre>
+  )
+}
+
+export default CodeMockupLine
