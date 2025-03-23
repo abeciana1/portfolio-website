3c6b3ce333d74ba31d3215df458ba2d2dc6c87bd
abeciana1 <alex.beciana@gmail.com>
2025-03-23 08:45:09 -0600

    Changed name

--- src/blocks/CodeMockupSection/config.ts
+++ src/blocks/CodeMockupSection/config.ts
@@ -1,6 +1,6 @@
 import { type Block } from 'payload';
 
-export const CodeMockupSection: Block = {
+export const CodeMockupSectionBlock: Block = {
   slug: 'code-mockup-section',
   interfaceName: 'CodeMockupSectionBlock',
   fields: [
