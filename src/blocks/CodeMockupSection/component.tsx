d1345b4c31d571992a175c98e93ad31999121031
abeciana1 <alex.beciana@gmail.com>
2025-03-23 08:44:09 -0600

    Created CodeMockupSection

---
+++ src/blocks/CodeMockupSection/component.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx'
+import { CodeMockupSectionProps } from '@/types/blockTypes'
+
+const CodeMockupSection: React.FC<CodeMockupSectionProps> = ({
+sectionId,
+  children,
+  enableSection,
+  background
+}) => {
+  return (
+    <section id={sectionId} tabIndex={0} className="my-10 relative">
+        <div className={clsx("mx-auto",{
+            ["mockup-code-section"]: enableSection,
+            ["bg-black text-white"]: background === "black",
+            ["bg-gray-200 text-black"]: background === "gray"
+        })}>
+            {children}
+        </div>
+    </section>
+  )
+}
+
+export default CodeMockupSection
