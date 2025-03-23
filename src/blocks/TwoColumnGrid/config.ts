abaa06a3871c8291df564d72cfb51d3fd11b5f14
abeciana1 <alex.beciana@gmail.com>
2025-03-23 11:26:22 -0600

    Created TwoColumnGrid block config

---
+++ src/blocks/TwoColumnGrid/config.ts
@@ -0,0 +1,39 @@
+import { type Block } from 'payload';
+import { CodeMockupSectionBlock } from '@/src/blocks/CodeMockupSection/config'
+
+const allowableBlocks = [
+  CodeMockupSectionBlock
+]
+
+export const TwoColumnGrid: Block = {
+  slug: 'two-column-grid',
+  interfaceName: 'TwoColumnGridBlock',
+  fields: [
+    {
+      name: 'sectionId',
+      type: 'text',
+      label: 'Section ID',
+      required: true,
+    },
+    {
+      name: 'column1',
+      type: 'blocks',
+      label: 'Column 1',
+      blocks: allowableBlocks
+    },
+    {
+      name: 'column2',
+      type: 'blocks',
+      label: 'Column 2',
+      blocks: allowableBlocks
+    },
+    {
+      name: 'reverseOrder',
+      type: 'checkbox',
+      label: 'Reverse Order',
+      admin: {
+        description: 'Reverses the order of the columns'
+      }
+    }
+  ]
+}
