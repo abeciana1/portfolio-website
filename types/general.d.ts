b1632a7a648fb49fe0ad74fac89ddab676ff47dc
abeciana1 <alex.beciana@gmail.com>
2025-03-23 08:29:50 -0600

    Removed comments

--- types/general.d.ts
+++ types/general.d.ts
@@ -1,16 +1,9 @@
 import { type Media } from '@/src/payload-types'
 
-/**
- * Your CMS media type – adds the custom webpUrl field
- */
 export type CMSMediaT = Media & {
   webpUrl: string
 }
 
-/**
- * If you need a type that also guarantees these UI-specific fields, you can define one.
- * (Often CMSMediaT is enough if you always add webpUrl, and your CMS already provides alt, width, etc.)
- */
 export type ImageT = CMSMediaT & {
   alt: string
   width: number
