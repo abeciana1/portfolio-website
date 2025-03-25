2c78a7ca12ee1639a5fdfba0de03fd93a3ae6cee
abeciana1 <alex.beciana@gmail.com>
2025-03-24 22:29:07 -0600

    Removed unused import

--- src/blocks/RichTextEditor/config.ts
+++ src/blocks/RichTextEditor/config.ts
@@ -1,15 +1,13 @@
 import { type Block } from 'payload'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 
 export const RichTextBlock: Block = {
   slug: 'rich-text-block',
   interfaceName: 'RichTextBlock',
   fields: [
     {
-      name: 'richTextContent',
+      name: 'content',
       type: 'richText',
       label: 'Rich Text Content',
-      editor: lexicalEditor(),
       required: true,
     },
   ],
