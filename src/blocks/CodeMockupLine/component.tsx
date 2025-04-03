17eecf8697b00c0c7e21f2898dbce1957427d0d1
abeciana1 <alex.beciana@gmail.com>
2025-04-02 20:51:01 -0600

    Added bolding

--- src/blocks/CodeMockupLine/component.tsx
+++ src/blocks/CodeMockupLine/component.tsx
@@ -7,13 +7,13 @@ const CodeMockupLine: React.FC<CodeMockupLineProps> = ({
   textColor,
 }) => {
   return (
-      <pre data-prefix={prefix} className={clsx("flex", {
+      <pre data-prefix={prefix} className={clsx("flex font-bold", {
           ["text-black"]: textColor === "black",
           ["text-white"]: textColor === "white",
           ["text-green-400"]: textColor === "success",
           ["text-[#F7B538]"]: textColor === "warning"
       })}>
-          <code className="whitespace-normal">
+          <code className="whitespace-normal font-bold">
               {text}
           </code>
       </pre>
