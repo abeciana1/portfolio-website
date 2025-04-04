18082e24750aa1f5d0034b9b2fab4028fa350fde
abeciana1 <alex.beciana@gmail.com>
2025-04-03 20:52:56 -0600

    Created InViewEmbed component

---
+++ src/blocks/InViewEmbed/component.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { type InViewEmbedProps } from '@/types/blockTypes'
+import { InView } from '@/components/_core/InView'
+import RenderBlocks from '@/src/blocks/RenderBlocks'
+
+const InViewEmbed: React.FC<InViewEmbedProps> = ({
+  sectionId,
+  embedBlocks,
+  hiddenY,
+  hiddenBlur,
+  visibleY,
+  visibleBlur,
+}) => {
+  return (
+    <section id={sectionId}>
+      <InView
+          variants={{
+            hidden: { opacity: 0, y: hiddenY, filter: `blur(${hiddenBlur}px)` },
+            visible: { opacity: 1, y: visibleY, filter: `blur(${visibleBlur}px)` },
+          }}
+          viewOptions={{ margin: '0px 0px -200px 0px' }}
+          transition={{ duration: 0.3, ease: 'easeInOut' }}
+      >
+        <RenderBlocks blocks={[...embedBlocks] as any} />
+      </InView>
+    </section>
+  )
+}
+
+export default InViewEmbed
