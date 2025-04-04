b738c4329dc7552241729205521dfef3bf8accb0
abeciana1 <alex.beciana@gmail.com>
2025-04-03 20:52:44 -0600

    Added in view fields

--- src/blocks/InViewEmbed/config.ts
+++ src/blocks/InViewEmbed/config.ts
@@ -20,6 +20,48 @@ export const InViewEmbedBlock: Block = {
       required: true,
     },
     {
+      label: 'Hidden options',
+      type: 'collapsible',
+      fields: [
+        {
+          name: 'hiddenY',
+          label: 'Hidden on y-axis',
+          type: 'number',
+          admin: {
+            description: 'Hidden on y-axis (in pixels)'
+          },
+          defaultValue: 0,
+        },
+        {
+          name: 'hiddenBlur',
+          label: 'Blur background',
+          type: 'number',
+          defaultValue: 4
+        }
+      ]
+    },
+    {
+      label: 'Visible options',
+      type: 'collapsible',
+      fields: [
+        {
+          name: 'visibleY',
+          label: 'Visible on y-axis',
+          type: 'number',
+          admin: {
+            description: 'Visible on y-axis (in pixels)'
+          },
+          defaultValue: 1,
+        },
+        {
+          name: 'visibleBlur',
+          label: 'Blur background',
+          type: 'number',
+          defaultValue: 0
+        }
+      ]
+    },
+    {
       name: 'embedBlocks',
       type: 'blocks',
       label: 'Embed Blocks',
