13818d826df30f6f55e4835f72342f20f8575a1e
abeciana1 <alex.beciana@gmail.com>
2025-04-03 20:39:16 -0600

    Created SkillsSection component

--- src/blocks/SkillsSection/component.tsx
+++ src/blocks/SkillsSection/component.tsx
@@ -1,6 +1,21 @@
-const SkillsSection = () => {
+import { type SkillsSectionProps } from '@/types/blockTypes'
+import { Heading1 } from '@/components/_styled/Heading'
+import { sectionContainer } from '@/utils/helpers'
+
+const SkillsSection: React.FC<SkillsSectionProps> = ({
+  sectionId,
+  heading,
+  skillsCollection
+}) => {
+  console.log('skillsCollection', skillsCollection)
   return (
-    <></>
+    <section
+      id={sectionId}
+      className={`${sectionContainer}`}
+    >
+      <Heading1 text={heading} />
+      <section className='py-5'></section>
+    </section>
   )
 }
 
