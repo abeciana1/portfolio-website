6e01b618f6a31befe15924058e19e73d2032fc90
abeciana1 <alex.beciana@gmail.com>
2025-04-04 22:32:35 -0600

    Added rendering of blocks

--- src/blocks/Card/component.tsx
+++ src/blocks/Card/component.tsx
@@ -1,6 +1,15 @@
-const Card = () => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CardBlockProps } from '@/types/blockTypes'
+import RenderBlocks from '@/src/blocks/RenderBlocks'
+
+const Card: React.FC<CardBlockProps> = ({
+  pill,
+  embedBlocks,
+}) => {
   return (
-    <></>
+    <div>
+      <RenderBlocks blocks={[...embedBlocks] as any} />
+    </div>
   )
 }
 
