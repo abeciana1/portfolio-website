199c9e30ad89bcbce8d0dfc67b56055cf1582f8e
abeciana1 <alex.beciana@gmail.com>
2025-04-04 22:02:52 -0600

    Adjusted padding

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -7,6 +7,6 @@ export const titleToSlug = (title: string) => {
 
 export const innerContainer = 'lg:px-20 lg:py-12 px-5 py-5'
 
-export const limitContainer = 'py-5 px-5 md:px-10 lg:py-24 md:justify-between'
+export const limitContainer = 'py-5 px-5 lg:py-24 md:justify-between'
 
-export const sectionContainer = 'py-5 px-5 md:px-10 lg:py-24'
+export const sectionContainer = 'py-12 px-5 lg:py-24'
