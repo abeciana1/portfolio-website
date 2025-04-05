a6ceefff4440d4bb6695c762cc9c225b6f9d71a3
abeciana1 <alex.beciana@gmail.com>
2025-04-04 22:10:23 -0600

    Created CardBlock configuration

---
+++ src/blocks/Card/config.ts
@@ -0,0 +1,25 @@
+import {
+  type Block,
+} from 'payload';
+import { CodeMockupSectionBlock } from '@/src/blocks/CodeMockupSection/config'
+import { RichTextBlock } from '@/src/blocks/RichTextEditor/config'
+import { TwoColumnGrid } from '@/src/blocks/TwoColumnGrid/config'
+
+const allowableBlocks = [
+  CodeMockupSectionBlock,
+  RichTextBlock,
+  TwoColumnGrid
+]
+
+export const CardBlock: Block = {
+  slug: 'card',
+  interfaceName: 'CardBlock',
+  fields: [
+    {
+      name: 'embedBlocks',
+      type: 'blocks',
+      label: 'Embed Blocks',
+      blocks: allowableBlocks,
+    }
+  ]
+}
