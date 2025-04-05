3de9d6a6beb389197b7b5c4a69340f4f2b260bf8
abeciana1 <alex.beciana@gmail.com>
2025-04-04 23:01:04 -0600

    Imported and added Pill component

--- src/blocks/Card/component.tsx
+++ src/blocks/Card/component.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { CardBlockProps } from '@/types/blockTypes'
 import RenderBlocks from '@/src/blocks/RenderBlocks'
+import Pill from '@/components/_styled/Pill'
 
 const Card: React.FC<CardBlockProps> = ({
   pill,
@@ -8,6 +9,7 @@ const Card: React.FC<CardBlockProps> = ({
 }) => {
   return (
     <div>
+      <Pill text={pill} />
       <RenderBlocks blocks={[...embedBlocks] as any} />
     </div>
   )
