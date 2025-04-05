fb35e0215aab4b53283aa181df09983561210255
abeciana1 <alex.beciana@gmail.com>
2025-04-04 22:03:43 -0600

    Added skill fetching

--- src/blocks/SkillsSection/component.tsx
+++ src/blocks/SkillsSection/component.tsx
@@ -1,13 +1,31 @@
 import { type SkillsSectionProps } from '@/types/blockTypes'
 import { Heading1 } from '@/components/_styled/Heading'
 import { sectionContainer } from '@/utils/helpers'
+import { payload } from '@/src/payload'
+import { QueryClient } from '@tanstack/react-query'
 
-const SkillsSection: React.FC<SkillsSectionProps> = ({
+const fetchSkillsList = async (queryClient: QueryClient, collectionId: number) => {
+  return await queryClient.ensureQueryData({
+    queryKey: ['skills'],
+    queryFn: () =>
+      payload.findByID({
+        collection: 'skills-collection',
+        id: collectionId,
+        depth: 2,
+        overrideAccess: true,
+      }),
+    staleTime: process.env.NODE_ENV === 'production' ? 60 * 1000 * 10 * 3 : 60 * 1000,
+  })
+}
+
+const SkillsSection: React.FC<SkillsSectionProps> = async ({
   sectionId,
   heading,
   skillsCollection
 }) => {
-  console.log('skillsCollection', skillsCollection)
+  const queryClient = new QueryClient()
+  const skillsContent = await fetchSkillsList(queryClient, skillsCollection?.id)
+  console.log('skillsContent', skillsContent)
   return (
     <section
       id={sectionId}
