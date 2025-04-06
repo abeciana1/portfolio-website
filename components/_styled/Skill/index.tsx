e2f2da08a0b9f440678aed395fa65e6ae18fdf97
abeciana1 <alex.beciana@gmail.com>
2025-04-06 15:01:53 -0600

    Removed width and height data

--- components/_styled/Skill/index.tsx
+++ components/_styled/Skill/index.tsx
@@ -1,17 +1,25 @@
 import { SkillProps } from '@/types/general'
+import Image from 'next/image'
 
 const Skill: React.FC<SkillProps> = ({
   title,
   skillIcon,
 }) => {
+  console.log('title', title)
   const {
     alt,
-    width,
-    height,
     webpUrl,
   } = skillIcon
   return (
-    <></>
+    <div className='relative'>
+        <Image
+          src={webpUrl}
+          alt={alt}
+          height={50}
+          width={50}
+        />
+        <div>{title}</div>
+    </div>
   )
 }
 
