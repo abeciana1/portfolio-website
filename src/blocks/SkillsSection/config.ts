42c940df7b128b76c1da4063114160492621ce5e
abeciana1 <alex.beciana@gmail.com>
2025-04-06 19:08:44 -0600

    Added gradient fields

--- src/blocks/SkillsSection/config.ts
+++ src/blocks/SkillsSection/config.ts
@@ -25,6 +25,54 @@ export const SkillsSection: Block = {
       type: 'relationship',
       relationTo: 'skills-collection' as CollectionSlug,
       required: true
+    },
+    {
+      name: 'gradient',
+      label: 'Add gradient to image',
+      type: 'checkbox',
+      admin: {
+        description: 'Add a gradient aura to the image'
+      },
+      defaultValue: false,
+    },
+    {
+      name: 'gradientXFlip',
+      label: 'Flip gradient along X-axis',
+      type: 'checkbox',
+      defaultValue: false
+    },
+    {
+      name: 'gradientYFlip',
+      label: 'Flip gradient along X-axis',
+      type: 'checkbox',
+      defaultValue: false
+    },
+    {
+      name: 'forcedWidth',
+      type: 'number',
+      label: 'Force image width (in pixels)',
+      admin: {
+        description: 'Set a specific width for the image, overriding the aspect ratio'
+      }
+    },
+    {
+      name: 'forcedHeight',
+      type: 'number',
+      label: 'Force image height (in pixels)',
+      admin: {
+        description: 'Set a specific height for the image, overriding the aspect ratio'
+      }
+    },
+    {
+      name: 'gradientSelect',
+      label: 'Gradient variant select',
+      type: 'select',
+      options: [
+        { label: 'Variant 1', value: 'Variant1' },
+        { label: 'Variant 2', value: 'Variant2' },
+        { label: 'Variant 3', value: 'Variant3' },
+        { label: 'Variant 4', value: 'Variant4' }
+      ]
     }
   ]
 }
