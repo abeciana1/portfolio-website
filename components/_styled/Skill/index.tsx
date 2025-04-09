2584f2c18112581331ffe4a95d97b44683f3769a
abeciana1 <alex.beciana@gmail.com>
2025-04-08 21:14:54 -0600

    Adjusted width and z-index

--- components/_styled/Skill/index.tsx
+++ components/_styled/Skill/index.tsx
@@ -10,22 +10,24 @@ const Skill: React.FC<SkillProps> = ({
     webpUrl,
   } = skillIcon
   return (
-    <div>
-      <div className="h-12 w-12 mx-auto">
-          <Image
-              width={50}
-              height={50}
-              src={webpUrl}
-              alt={`Alex Beciana | Skill - ${alt}`}
-              className="mx-auto dark:bg-background p-0.5"
-          />
+    <div className='max-w-fit'>
+      <div className='z-50 bg-pillGrey px-2.5 py-2 rounded-lg w-44 h-44 flex flex-col justify-center items-center'>
+        <div className="z-50 h-12 w-12 mx-auto">
+            <Image
+                width={50}
+                height={50}
+                src={webpUrl}
+                alt={`Alex Beciana | Skill - ${alt}`}
+                className="mx-auto p-0.5"
+            />
+        </div>
+        <div
+          className="mt-4 text-center dark:text-foreground"
+        >
+          {title}
+        </div>
       </div>
-      <div
-        className="mt-4 text-center"
-      >
-        {title}
-      </div>
-  </div>
+    </div>
   )
 }
 
