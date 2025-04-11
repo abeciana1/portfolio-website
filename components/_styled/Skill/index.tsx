8482fb02fe033bc148a76e3e963e94d989d7cdbb
abeciana1 <alex.beciana@gmail.com>
2025-04-10 21:22:41 -0600

    Adjusted dimensions

--- components/_styled/Skill/index.tsx
+++ components/_styled/Skill/index.tsx
@@ -11,7 +11,7 @@ const Skill: React.FC<SkillProps> = ({
   } = skillIcon
   return (
     <div className='max-w-fit'>
-      <div className='z-50 bg-pillGrey px-2.5 py-2 rounded-lg w-44 h-44 flex flex-col justify-center items-center'>
+      <div className='z-50 bg-pillGrey px-2.5 py-2 rounded-lg w-32 h-32 lg:w-44 lg:h-44 flex flex-col justify-center items-center'>
         <div className="z-50 h-12 w-12 mx-auto">
             <Image
                 width={50}
