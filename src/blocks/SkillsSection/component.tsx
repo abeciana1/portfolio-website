2e2e6ccac67cd46b07a0926ecbdf451be5204439
abeciana1 <alex.beciana@gmail.com>
2025-04-10 19:45:45 -0600

    Added z-index

--- src/blocks/SkillsSection/component.tsx
+++ src/blocks/SkillsSection/component.tsx
@@ -40,15 +40,17 @@ const SkillsSection: React.FC<SkillsSectionProps> = async ({
         id={sectionId}
         className={`relative w-full ${sectionContainer}`}
       >
-        <Heading1 text={heading} />
+        <div className='relative z-[9999] text-center'>
+          <Heading1 text={heading} />
+        </div>
+        {(gradient && gradientSelect) &&
+          <div className='z-0 absolute top-10 left-5 rounded-full h-56 w-56 sm:h-[25rem] sm:w-[30rem] md:h-[20rem] md:w-[50rem] overflow-hidden blur-3xl opacity-50'>
+            <Gradient
+              variant={gradientSelect as GradientOptions}
+            />
+          </div>
+        }
         <div className="z-50 relative grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-6 max-w-fit mx-auto mt-6">
-          {(gradient && gradientSelect) &&
-            <div className='-z-10 absolute top-0 left-5 sm:top-2 md:-top-5 sm:left-10 md:left-20 rounded-full h-56 w-56 sm:h-[25rem] sm:w-[30rem] md:h-[20rem] md:w-[40rem] overflow-hidden blur-3xl'>
-              <Gradient
-                variant={gradientSelect as GradientOptions}
-              />
-            </div>
-          }
           {skillsContent?.skills && skillsContent?.skills
           .filter((skill): skill is SkillType => typeof skill !== 'number')
           .map((skill: SkillType) => (
@@ -58,15 +60,6 @@ const SkillsSection: React.FC<SkillsSectionProps> = async ({
               skillIcon={skill.skillIcon as CMSMediaT}
             />
           ))}
-          {(gradient && gradientSelect) &&
-            <div className="-z-10 absolute bottom-0 right-5 sm:bottom-2 md:bottom-32 sm:right-10 md:right-20 rounded-full h-56 w-56 sm:h-[25rem] sm:w-[35rem] md:h-[20rem] md:w-[40rem] overflow-hidden blur-3xl">
-              <Gradient 
-                variant={gradientSelect as GradientOptions}
-                gradientXFlip={false} 
-                gradientYFlip={true} 
-              />
-            </div>
-          }
         </div>
       </section>
     </>
