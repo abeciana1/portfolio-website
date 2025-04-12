2de2b8c2764755fb05bdd76d0790e8642b32dec0
abeciana1 <alex.beciana@gmail.com>
2025-04-11 19:39:13 -0600

    Added props

--- src/blocks/CallToAction/component.tsx
+++ src/blocks/CallToAction/component.tsx
@@ -1,4 +1,12 @@
-const CallToAction = () => {
+import Link from 'next/link'
+import { type CallToActionProps } from '@/types/blockTypes'
+
+const CallToAction: React.FC<CallToActionProps> = ({
+  style,
+  arrow,
+  arrowDirection,
+  link
+}) => {
   return (
     <></>
   )
