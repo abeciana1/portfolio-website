5457c172ad71d43ea8f112d34bf34eaa5a761100
abeciana1 <alex.beciana@gmail.com>
2025-04-14 21:51:44 -0600

    Created job experience block schema

---
+++ src/blocks/Job/config.ts
@@ -0,0 +1,96 @@
+import { type Block } from 'payload';
+import { RichTextBlock } from '@/src/blocks/RichTextEditor/config'
+import { Image } from '@/src/blocks/Image/config'
+
+export const Job: Block = {
+  slug: 'job-block',
+  interfaceName: 'JobBlock',
+  fields: [
+    {
+      name: 'companyLogo',
+      label: 'Company logo',
+      type: 'blocks',
+      maxRows: 1,
+      blocks: [
+        Image
+      ],
+      required: true
+    },
+    {
+      name: 'companyName',
+      label: 'Company',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'jobRole',
+      label: 'Role',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'startDate',
+      label: 'Start date',
+      type: 'date',
+      required: true
+    },
+    {
+      name: 'currentPosition',
+      label: 'Current position?',
+      type: 'checkbox'
+    },
+    {
+      name: 'endDate',
+      label: 'End date',
+      type: 'date',
+      required: true,
+      admin: {
+        condition: (data) => {
+          if (!data.currentPosition) {
+            return false
+          } else {
+            return true
+          }
+        }
+      }
+    },
+    {
+      name: 'location',
+      label: 'Location',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'positionType',
+      label: 'Type of position',
+      type: 'select',
+      options: [
+        { label: 'Contract', value: 'Contract' },
+        { label: 'Full-Time', value: 'Full-Time' },
+        { label: 'Entrepreneur', value: 'Entrepreneur' }
+      ]
+    },
+    {
+      name: 'companyDescription',
+      label: 'Company description',
+      type: 'textarea',
+      required: true
+    },
+    {
+      name: 'companyWebsite',
+      label: 'Company website',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'duties',
+      label: 'Job responsibilities',
+      type: 'blocks',
+      maxRows: 1,
+      blocks: [
+        RichTextBlock
+      ],
+      required: true
+    }
+  ]
+}
