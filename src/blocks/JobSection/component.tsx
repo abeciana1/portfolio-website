3280fbfbce4be2adf8bebcc17de8cc61ec88345d
abeciana1 <alex.beciana@gmail.com>
2025-04-17 21:53:28 -0600

    Started content mapping

--- src/blocks/JobSection/component.tsx
+++ src/blocks/JobSection/component.tsx
@@ -1,4 +1,6 @@
 import { type JobSectionProps } from '@/types/blockTypes'
+import { Heading1 } from '@/components/_styled/Heading'
+import Pill from '@/components/_styled/Pill'
 
 const JobSection: React.FC<JobSectionProps> = ({
   title,
@@ -7,7 +9,17 @@ const JobSection: React.FC<JobSectionProps> = ({
   gradientSelect
 }) => {
   return (
-    <section></section>
+    <section className='flex flex-col space-y-6'>
+      <Pill text='Experience' />
+      <Heading1 text={title} />
+      {/* <div className='flex flex-col space-y-6'>
+      </div> */}
+      {jobs && jobs.map((job, index) => {
+        return (
+          <></>
+        )
+      })}
+    </section>
   )
 }
 
