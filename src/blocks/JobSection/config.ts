d98c97c4d1d0fa4944447e276291f9561489130e
abeciana1 <alex.beciana@gmail.com>
2025-04-17 20:48:20 -0600

    Added gradient fields

--- src/blocks/JobSection/config.ts
+++ src/blocks/JobSection/config.ts
@@ -12,6 +12,26 @@ export const JobSection: Block = {
       blocks: [
         Job
       ]
+    },
+    {
+      name: 'gradient',
+      label: 'Add gradient to image',
+      type: 'checkbox',
+      admin: {
+        description: 'Add a gradient aura to the image'
+      },
+      defaultValue: false,
+    },
+    {
+      name: 'gradientSelect',
+      label: 'Gradient variant select',
+      type: 'select',
+      options: [
+        { label: 'Variant 1', value: 'Variant1' },
+        { label: 'Variant 2', value: 'Variant2' },
+        { label: 'Variant 3', value: 'Variant3' },
+        { label: 'Variant 4', value: 'Variant4' }
+      ]
     }
   ]
 }
