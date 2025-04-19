ad32a8f83833e09976b060522d931e474a03430f
abeciana1 <alex.beciana@gmail.com>
2025-04-19 11:10:10 -0600

    Created failing tests

--- src/blocks/Job/__test__/index.test.tsx
+++ src/blocks/Job/__test__/index.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Job from '@/src/blocks/Job/component'
+import userEvent from '@testing-library/user-event';
+
 
 describe('Job', () => {
   beforeEach(() => {
@@ -52,5 +54,55 @@ describe('Job', () => {
         ]}
       />
     )
+  })
+  test('should render company name', () => {
+    const companyName = screen.getByTestId('company-name')
+    expect(companyName).toBeInTheDocument()
+    expect(companyName.innerText).toBe('Company')
+  })
+  test('should render job role', () => {
+    const role = screen.getByTestId('job-role')
+    expect(role.innerText).toBe('Developer')
+    expect(role).toBeInTheDocument()
+  })
+  test('should render position', () => {
+    const position = screen.getByTestId('position')
+    expect(position.innerText).toBe('Contract')
+    expect(position).toBeInTheDocument()
+  })
+  test('should render company description', async () => {
+    const jobEl = screen.getByTestId('job')
+    await userEvent.click(jobEl)
+    const description = await screen.findByTestId('company-desc')
+    expect(description.innerText).toBe('Description')
+    expect(description).toBeInTheDocument()
+  })
+  test('should render company website', async () => {
+    const jobEl = screen.getByTestId('job')
+    await userEvent.click(jobEl)
+    const compWeb = await screen.findByRole('link', {
+      name: 'company website'
+    })
+    expect(compWeb).toHaveAttribute('href', 'https://example.com')
+    expect(compWeb).toHaveAccessibleName('company website')
+    expect(compWeb).toBeInTheDocument()
+  })
+  test('should render location', () => {
+    const location = screen.getByTestId('location')
+    expect(location.innerText).toBe('Remote')
+    expect(location).toBeInTheDocument()
+  })
+  test('should render company logo', () => {
+    const logo = screen.getByRole('img')
+    expect(logo).toBeInTheDocument()
+    expect(logo).toHaveAttribute('alt', 'Company logo - Company')
+  })
+  test('should render job duties', async () => {
+    const jobEl = screen.getByTestId('job')
+    await userEvent.click(jobEl)
+    const duties = await screen.findByTestId('duties')
+    expect(duties).toBeInTheDocument()
+    expect(duties.innerHTML).toBe('<h1>hello</h1>')
+    expect(duties.innerText).toBe('hello')
   })
 })
