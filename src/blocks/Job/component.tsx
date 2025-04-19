e7836bed3336c1460b638bc116f31db287186d9c
abeciana1 <alex.beciana@gmail.com>
2025-04-19 16:46:17 -0600

    Added content

--- src/blocks/Job/component.tsx
+++ src/blocks/Job/component.tsx
@@ -25,10 +25,51 @@ const Job: React.FC<JobProps> = ({
   skills,
   duties
 }) => {
+  const { image } = companyLogo[0]
+  const { webpUrl } = image
   return (
-    <div data-testid='job'>
-
-    </div>
+    <MorphingDialog
+      testId='job'
+      transition={{
+        type: 'spring',
+        stiffness: 200,
+        damping: 24,
+      }}
+    >
+      <MorphingDialogTrigger
+        className='mx-auto border-2 border-darkGrey dark:border-pillGrey rounded-lg'
+      >
+        <div className='flex space-x-6'>
+          <MorphingDialogImage
+            src={webpUrl}
+            alt={`Company logo - ${companyName}`}
+            className='h-8 w-8 object-cover object-top rounded-lg'
+          />
+          <div className='flex flex-col'>
+            <MorphingDialogTitle
+              className='flex justify-between text-foreground dark:text-background font-medium text-md'
+            >
+              <div>
+                <span data-testid='job-role'>{jobRole}</span>
+                <span>·</span>
+                <span data-testid='company-name'>{companyName}</span>
+              </div>
+              <div>
+                <span>{`${startDate} — ${currentPosition ? 'Present' : endDate}`}</span>
+              </div>
+            </MorphingDialogTitle>
+            <MorphingDialogSubtitle
+              className='flex justify-between text-darkGrey dark:text-pillGrey text-md'
+            >
+              <div>
+                <span data-testid='position'>{positionType}</span>
+                <span>{location}</span>
+              </div>
+            </MorphingDialogSubtitle>
+          </div>
+        </div>
+      </MorphingDialogTrigger>
+    </MorphingDialog>
   )
 }
 
