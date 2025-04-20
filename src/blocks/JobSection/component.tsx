bde6785c5395982e1334a397e2d43bbf8af85ced
abeciana1 <alex.beciana@gmail.com>
2025-04-20 11:28:50 -0600

    Added mapping for jobs data

--- src/blocks/JobSection/component.tsx
+++ src/blocks/JobSection/component.tsx
@@ -1,6 +1,11 @@
-import { type JobSectionProps } from '@/types/blockTypes'
+import {
+  type JobSectionProps,
+  type ImageBlockProps,
+  type JobProps
+} from '@/types/blockTypes'
 import { Heading1 } from '@/components/_styled/Heading'
 import Pill from '@/components/_styled/Pill'
+import Job from '@/src/blocks/Job/component'
 
 const JobSection: React.FC<JobSectionProps> = ({
   sectionId,
@@ -15,9 +20,23 @@ const JobSection: React.FC<JobSectionProps> = ({
       <Heading1 text={title} />
       {/* <div className='flex flex-col space-y-6'>
       </div> */}
-      {jobs && jobs.map((job, index) => {
+      {jobs && jobs.map((job: JobProps, index: number) => {
         return (
-          <></>
+          <Job
+            key={job.id || `${job.companyName}-${index}`}
+            companyDescription={job.companyDescription}
+            companyName={job.companyName}
+            companyLogo={job.companyLogo}
+            companyWebsite={job.companyWebsite}
+            jobRole={job.jobRole}
+            startDate={job.startDate}
+            endDate={job.endDate}
+            location={job.location}
+            positionType={job.positionType}
+            currentPosition={job.currentPosition}
+            skills={job.skills}
+            duties={job.duties}
+          />
         )
       })}
     </section>
