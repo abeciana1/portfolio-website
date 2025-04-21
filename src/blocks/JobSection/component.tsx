54f9752e4e995d70f736c181dfa4c9639e5784c7
abeciana1 <alex.beciana@gmail.com>
2025-04-20 20:35:14 -0600

    Added rendering of gradient

--- src/blocks/JobSection/component.tsx
+++ src/blocks/JobSection/component.tsx
@@ -1,10 +1,12 @@
 import {
   type JobSectionProps,
-  type JobProps
+  type JobProps,
+  type GradientOptions
 } from '@/types/blockTypes'
 import { Heading1 } from '@/components/_styled/Heading'
 import Pill from '@/components/_styled/Pill'
 import Job from '@/src/blocks/Job/component'
+import Gradient from '@/components/_styled/Gradient'
 
 const JobSection: React.FC<JobSectionProps> = ({
   sectionId,
@@ -14,8 +16,19 @@ const JobSection: React.FC<JobSectionProps> = ({
   gradientSelect
 }) => {
   return (
-    <section id={sectionId} className='flex flex-col space-y-6 justify-center items-center lg:py-24 px-5 py-10'>
-      <Pill text='Experience' />
+    <section id={sectionId} className='relative z-20 flex flex-col space-y-6 justify-center items-center lg:py-24 px-5 py-10'>
+      {gradient &&
+        <div
+          className='z-0 absolute top-5 right-0 md:right-8 rounded-full h-56 sm:h-[10rem] md:h-[20rem] w-[45rem] overflow-hidden blur-3xl sm:opacity-70'
+        >
+          <Gradient
+            variant={gradientSelect as GradientOptions}
+          />
+        </div>
+      }
+      <div className='z-20'>
+        <Pill text='Experience' />
+      </div>
       <Heading1 text={title} />
       {jobs && jobs.map((job: JobProps, index: number) => {
         return (
