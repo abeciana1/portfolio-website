8b2b2b43e959c41e0fc4ce24410e991bf2675b20
abeciana1 <alex.beciana@gmail.com>
2025-04-20 20:33:42 -0600

    Adjusted z-index

--- components/_styled/Skill/index.tsx
+++ components/_styled/Skill/index.tsx
@@ -11,8 +11,8 @@ const Skill: React.FC<SkillProps> = ({
   } = skillIcon
   return (
     <div className='max-w-fit'>
-      <div className='z-50 bg-pillGrey px-2.5 py-2 rounded-lg w-32 h-32 lg:w-44 lg:h-44 flex flex-col justify-center items-center'>
-        <div className="z-50 h-12 w-12 mx-auto">
+      <div className='z-20 bg-pillGrey px-2.5 py-2 rounded-lg w-32 h-32 lg:w-44 lg:h-44 flex flex-col justify-center items-center'>
+        <div className="z-20 h-12 w-12 mx-auto">
             <Image
                 width={50}
                 height={50}
