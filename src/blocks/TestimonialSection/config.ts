6541d4ceb35c9682d9987eafa0b1ddab9d7f9c67
abeciana1 <alex.beciana@gmail.com>
2025-05-01 20:31:01 -0600

    Created TestimonialSectionBlock schema

---
+++ src/blocks/TestimonialSection/config.ts
@@ -0,0 +1,34 @@
+import {
+  type Block,
+  type CollectionSlug
+} from 'payload';
+
+export const TestimonialSectionBlock: Block = {
+  slug: 'testimonial-section-block',
+  interfaceName: 'TestimonialSectionBlock',
+  fields: [
+    {
+      name: 'sectionId',
+      type: 'text',
+      label: 'Section ID',
+    },
+    {
+      name: 'pill',
+      type: 'text',
+      label: 'Pill',
+    },
+    {
+      name: 'heading',
+      type: 'text',
+      label: 'Heading',
+      required: true,
+    },
+    {
+      name: 'testimonials',
+      label: 'Testimonials',
+      type: 'relationship',
+      relationTo: 'testimonials' as CollectionSlug,
+      required: true
+    }
+  ]
+}
