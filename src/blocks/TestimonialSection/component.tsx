a5d4199748a57c23fce10ffd1f28a85eaf4c0a1d
abeciana1 <alex.beciana@gmail.com>
2025-05-01 20:51:00 -0600

    Created initial render for TestimonialSection

---
+++ src/blocks/TestimonialSection/component.tsx
@@ -0,0 +1,11 @@
+import {
+  type TestimonialSectionProps
+} from '@/types/blockTypes'
+
+const TestimonialSection: React.FC<TestimonialSectionProps> = () => {
+  return (
+    <></>
+  )
+}
+
+export default TestimonialSection
