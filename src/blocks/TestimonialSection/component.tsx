cdcd2e0020e5458f7c7203fe17ed33a263339552
abeciana1 <alex.beciana@gmail.com>
2025-05-01 21:00:49 -0600

    Added deconstructed props

--- src/blocks/TestimonialSection/component.tsx
+++ src/blocks/TestimonialSection/component.tsx
@@ -2,9 +2,15 @@ import {
   type TestimonialSectionProps
 } from '@/types/blockTypes'
 
-const TestimonialSection: React.FC<TestimonialSectionProps> = () => {
+const TestimonialSection: React.FC<TestimonialSectionProps> = ({
+  sectionId,
+  pill,
+  heading,
+  description,
+  testimonials
+}) => {
   return (
-    <></>
+    <section></section>
   )
 }
 
