8e571105a9a0d24e1440f23db4a2e8ded5986aea
abeciana1 <alex.beciana@gmail.com>
2025-05-08 21:04:12 -0600

    Added trigger closed content rendering

--- components/_styled/Testimonial/index.tsx
+++ components/_styled/Testimonial/index.tsx
@@ -31,7 +31,27 @@ const Testimonal: React.FC<TestimonialProps> = ({
         testId='testimonial'
         className='relative w-full max-w-2xl mx-auto border-2 border-darkGrey dark:border-pillGrey rounded-lg p-4 md:h-20 bg-background'
       >
-
+        <div className='flex space-x-6 items-center'>
+          <MorphingDialogImage
+            src={headshot?.image?.webpUrl as string}
+            alt={`${name} headshot`}
+            className='h-auto w-auto max-h-20 max-w-20 min-w-[80px] object-cover object-top'
+          />
+          <div className='flex flex-col w-full'>
+            <div className='flex flex-col sm:flex-row justify-between text-foreground font-medium text-md'>
+              <MorphingDialogTitle>
+                <div className='flex flex-col sm:flex-row sm:gap-2 text-left'>
+                  <span data-testid='job-role'>{name}</span>
+                </div>
+              </MorphingDialogTitle>
+            </div>
+            <MorphingDialogSubtitle
+              className='flex justify-between text-darkGrey text-md'
+            >
+              <span data-testid='position'>{`${position} @ ${company}`}</span>
+            </MorphingDialogSubtitle>
+          </div>
+        </div>
       </MorphingDialogTrigger>
     </MorphingDialog>
   )
