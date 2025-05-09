6baa8b272b53aad3f4edda0eb7a8b631448a5b6e
abeciana1 <alex.beciana@gmail.com>
2025-05-08 20:01:32 -0600

    Added pill, heading, and description rendering

--- src/blocks/TestimonialSection/component.tsx
+++ src/blocks/TestimonialSection/component.tsx
@@ -1,6 +1,9 @@
 import {
   type TestimonialSectionProps
 } from '@/types/blockTypes'
+import { Carousel } from '@/components/motion-primitives/carousel'
+import Pill from '@/components/_styled/Pill'
+import { Heading1 } from '@/components/_styled/Heading'
 
 const TestimonialSection: React.FC<TestimonialSectionProps> = ({
   sectionId,
@@ -10,7 +13,20 @@ const TestimonialSection: React.FC<TestimonialSectionProps> = ({
   testimonials
 }) => {
   return (
-    <section></section>
+    <section
+      data-testid={sectionId as string}
+      id={sectionId as string}
+    >
+      <div className='relative flex flex-col gap-6 z-40 text-center md:max-w-2xl mx-auto'>
+        <div className='flex justify-center'>
+          <Pill text={pill as string} />
+        </div>
+        <Heading1 text={heading} />
+        <div className='text-darkGrey dark:text-pillGrey text-xl font-semibold'>
+          {description}
+        </div>
+      </div>
+    </section>
   )
 }
 
