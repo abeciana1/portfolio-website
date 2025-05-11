86f0f639ae9af24717ad8aaabbe56386494ca557
abeciana1 <alex.beciana@gmail.com>
2025-05-11 09:13:08 -0600

    Added gradient conditional field

--- src/blocks/TestimonialSection/config.ts
+++ src/blocks/TestimonialSection/config.ts
@@ -35,6 +35,35 @@ export const TestimonialSectionBlock: Block = {
       relationTo: 'testimonials' as CollectionSlug,
       required: true,
       hasMany: true
+    },
+    {
+      name: 'gradient',
+      label: 'Add gradient to image',
+      type: 'checkbox',
+      admin: {
+        description: 'Add a gradient aura to the image'
+      },
+      defaultValue: false,
+    },
+    {
+      name: 'gradientSelect',
+      label: 'Gradient variant select',
+      type: 'select',
+      options: [
+        { label: 'Variant 1', value: 'Variant1' },
+        { label: 'Variant 2', value: 'Variant2' },
+        { label: 'Variant 3', value: 'Variant3' },
+        { label: 'Variant 4', value: 'Variant4' }
+      ],
+      admin: {
+        condition: (data, siblingData) => {
+          if (siblingData.gradient) {
+            return true
+          } else {
+            return false
+          }
+        }
+      }
     }
   ]
 }
