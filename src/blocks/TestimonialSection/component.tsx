1e3ed1b47b15dcfbb8eece0a73445f94b06711de
abeciana1 <alex.beciana@gmail.com>
2025-05-11 21:12:14 -0600

    Adjusted gradient positioning

--- src/blocks/TestimonialSection/component.tsx
+++ src/blocks/TestimonialSection/component.tsx
@@ -35,7 +35,7 @@ const TestimonialSection: React.FC<TestimonialSectionProps> = ({
     >
       {gradient &&
         <div
-          className='z-0 absolute -top-15 right-0 md:right-8 rounded-full h-56 sm:h-[10rem] md:h-[20rem] w-[45rem] overflow-hidden blur-3xl sm:opacity-70'
+          className='z-0 absolute top-1/5 left-1/5 rounded-full h-64 sm:h-[10rem] md:h-[20rem] w-[45rem] overflow-hidden blur-3xl sm:opacity-70'
         >
           <Gradient
             variant={gradientSelect as GradientOptions}
