304d46e81d963c57baa5e7943c48198f1791640d
abeciana1 <alex.beciana@gmail.com>
2025-05-14 21:10:42 -0600

    Adjusted function to replace ()

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,8 +1,9 @@
 export const titleToSlug = (title: string) => {
   return title
-  .replace(/([a-z])([A-Z])/g, '$1 $2')
-  .replace(/[\s_]+/g, '-')
-  .toLowerCase(); 
+    .replace(/[()]/g, '') // Remove parentheses
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .replace(/[\s_]+/g, '-')
+    .toLowerCase();
 }
 
 export const innerContainer = 'lg:px-20 lg:py-12 md:px-10 px-5 py-5'
