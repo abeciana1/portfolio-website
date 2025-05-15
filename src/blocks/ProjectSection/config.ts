474fc6bf0235cf549edb86f27dfa90d09d4fc996
abeciana1 <alex.beciana@gmail.com>
2025-05-14 21:35:24 -0600

    Added config for project section

---
+++ src/blocks/ProjectSection/config.ts
@@ -0,0 +1,48 @@
+import { type Block } from 'payload';
+import { CallToAction } from '@/src/blocks/CallToAction/config'
+
+export const ProjectSection: Block = {
+  slug: 'project-section-block',
+  interfaceName: 'ProjectSectionBlock',
+  fields: [
+    {
+      name: 'sectionId',
+      type: 'text',
+      label: 'Section ID',
+      required: true,
+    },
+    {
+      name: 'pill',
+      type: 'text',
+      label: 'Pill',
+    },
+    {
+      name: 'heading',
+      type: 'text',
+      label: 'Heading',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'text',
+      label: 'Description'
+    },
+    {
+      name: 'callToAction',
+      label: 'Call-to-Action',
+      type: 'blocks',
+      maxRows: 2,
+      blocks: [
+        CallToAction
+      ],
+      required: false,
+    },
+    {
+      name: 'projects',
+      label: 'Projects',
+      type: 'relationship',
+      relationTo: 'projects',
+      required: true
+    }
+  ]
+}
