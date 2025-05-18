8a02edc2ff10454848cb509065186536f47c71f6
abeciana1 <alex.beciana@gmail.com>
2025-05-17 21:27:45 -0600

    Copied over pages collection - adjusted project-pages

---
+++ src/app/(frontend)/projects/page.tsx
@@ -0,0 +1,64 @@
+import type { Metadata } from 'next'
+import {
+  getPayload,
+  type RequiredDataFromCollectionSlug
+} from 'payload'
+import { cache } from 'react'
+import RenderBlocks from '@/src/blocks/RenderBlocks'
+import { notFound } from 'next/navigation';
+import buildConfig from '@/src/payload.config'
+import { generateMeta } from '@/utils/generateMeta'
+
+type Args = {
+  params: Promise<{
+    slug?: string
+  }>
+}
+
+const Page = async ({ params: paramsPromise }: Args) => {
+  const { slug = 'home' } = await paramsPromise
+
+  const page: RequiredDataFromCollectionSlug<'project-pages'> | null = await queryPageBySlug({
+    slug,
+  })
+
+  if (!page) {
+    return notFound()
+  }
+
+  const { layout } = page
+
+  return (
+    <main className='relative overflow-x-hidden'>
+      <RenderBlocks blocks={layout} />
+    </main>
+  )
+}
+
+const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
+  const payload = await getPayload({ config: buildConfig })
+
+  const result = await payload.find({
+    collection: 'project-pages',
+    limit: 1,
+    pagination: false,
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  })
+
+  return result.docs?.[0] || null
+})
+
+export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
+  const { slug = 'home' } = await paramsPromise
+  const page = await queryPageBySlug({
+    slug,
+  })
+
+  return generateMeta({ doc: page })
+}
+
+export default Page
