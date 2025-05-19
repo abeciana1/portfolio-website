bf346e16a3c6b16c613b363a8d53a89bccacb1f7
abeciana1 <alex.beciana@gmail.com>
2025-05-18 18:30:27 -0600

    Wrapped render func in beforeEach

--- src/blocks/TestimonialSection/__test__/index.test.tsx
+++ src/blocks/TestimonialSection/__test__/index.test.tsx
@@ -3,35 +3,37 @@ import '@testing-library/jest-dom'
 import TestimonialSection from '../component'
 
 describe('TestimonialSection', () => {
-  render(
-    <TestimonialSection
-      sectionId='testimonials'
-      pill='Testionials'
-      heading='Testimonials'
-      description='Testimonials description'
-      gradient={false}
-      carouselCustom={false}
-      enableInfinite={false}
-      carouselTimer={false}
-      secondsTimer={0}
-      testimonials={[
-        {
-          name: 'name',
-          position: 'position',
-          company: 'company',
-          headshot: {
-            id: 1,
-            width: 100,
-            height: 100,
-            alt: 'testimonial headshot',
-            webpUrl: 'https://example.com',
-            updatedAt: '',
-            createdAt: ''
-          },
-          callout: 'callout',
-          content_html: ''
-        }
-      ]}
-    />
-  )
+  beforeEach(() => {
+    render(
+      <TestimonialSection
+        sectionId='testimonials'
+        pill='Testionials'
+        heading='Testimonials'
+        description='Testimonials description'
+        gradient={false}
+        carouselCustom={false}
+        enableInfinite={false}
+        carouselTimer={false}
+        secondsTimer={0}
+        testimonials={[
+          {
+            name: 'name',
+            position: 'position',
+            company: 'company',
+            headshot: {
+              id: 1,
+              width: 100,
+              height: 100,
+              alt: 'testimonial headshot',
+              webpUrl: 'https://example.com',
+              updatedAt: '',
+              createdAt: ''
+            },
+            callout: 'callout',
+            content_html: ''
+          }
+        ]}
+      />
+    )
+  })
 })
