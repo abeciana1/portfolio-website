f53af77c99ca1c0d5e47057b3cc377ea82c28001
abeciana1 <alex.beciana@gmail.com>
2025-05-19 21:28:01 -0600

    Created initial render for ProjectSection

---
+++ src/blocks/ProjectSection/component.tsx
@@ -0,0 +1,11 @@
+import Pill from '@/components/_styled/Pill'
+import { Heading1 } from '@/components/_styled/Heading'
+import { type ProjectSectionBlockProps } from '@/types/blockTypes'
+
+const ProjectSection = () => {
+  return (
+    <section></section>
+  )
+}
+
+export default ProjectSection
