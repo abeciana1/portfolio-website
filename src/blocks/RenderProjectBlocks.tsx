3ee1bdc11c315c7d434f0c9332f0e2aa30268e6b
abeciana1 <alex.beciana@gmail.com>
2025-05-19 21:07:33 -0600

    Created dynamic project blocks

---
+++ src/blocks/RenderProjectBlocks.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { type ProjectPage } from '@/src/payload-types'
+import { Fragment } from 'react'
+import { transformBlockMedia } from '@/utils/transformImage'
+
+// * components
+import HeroSectionNoImage from '@/src/blocks/HeroSectionNoImage/component'
+
+// * component types
+import {
+  type HeroSectionNoImageBlockProps
+} from '@/types/blockTypes'
+
+export type BlockComponentsMap = {
+  'hero-section-no-image': React.FC<HeroSectionNoImageBlockProps>;
+};
+
+const blockComponents: BlockComponentsMap = {
+  'hero-section-no-image': HeroSectionNoImage
+}
+
+const RenderProjectBlocks: React.FC<{
+  blocks: ProjectPage['layout']
+}> = (props) => {
+  const { blocks } = props
+  const hasBlocks = blocks && Array.isArray(blocks) && blocks.length > 0
+
+  if (hasBlocks) {
+    return (
+      <Fragment>
+        {blocks.map((block, index) => {
+          if (!block.blockType || !(block.blockType in blockComponents)) {
+            return null
+          }
+          const Block = blockComponents[block.blockType]
+          // Automatically transform media if it exists.
+          const transformedBlock = transformBlockMedia(block)
+          // Type assertion may be needed here if TypeScript loses track of the exact type.
+          return <Block key={index} {...(transformedBlock as any)} />
+        })}
+      </Fragment>
+    )
+  }
+  return null
+}
+
+export default RenderProjectBlocks
