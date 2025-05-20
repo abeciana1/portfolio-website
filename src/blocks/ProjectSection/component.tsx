c2d04c62cced42671fec90149e5763ab34a065b5
abeciana1 <alex.beciana@gmail.com>
2025-05-19 21:30:15 -0600

    Added props

--- src/blocks/ProjectSection/component.tsx
+++ src/blocks/ProjectSection/component.tsx
@@ -2,9 +2,20 @@ import Pill from '@/components/_styled/Pill'
 import { Heading1 } from '@/components/_styled/Heading'
 import { type ProjectSectionBlockProps } from '@/types/blockTypes'
 
-const ProjectSection = () => {
+const ProjectSection: React.FC<ProjectSectionBlockProps> = ({
+  sectionId,
+  pill,
+  heading,
+  description,
+  callToAction,
+  projects
+}) => {
   return (
-    <section></section>
+    <section
+      data-testid={sectionId as string}
+      id={sectionId as string}
+      className='relative z-50 lg:py-24 px-5 py-10'
+    ></section>
   )
 }
 
