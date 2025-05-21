181b64b04fe21efcbb8cad8ede15395f40fad27a
abeciana1 <alex.beciana@gmail.com>
2025-05-20 19:33:09 -0600

    Created component

---
+++ src/blocks/ProjectGrid/component.tsx
@@ -0,0 +1,11 @@
+import { type ProjectSectionBlockProps } from '@/types/blockTypes'
+
+const ProjectGrid: React.FC<Pick<ProjectSectionBlockProps, 'projects'>> = ({
+  projects
+}) => {
+  return (
+    <section></section>
+  )
+}
+
+export default ProjectGrid
