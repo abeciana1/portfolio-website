cac0d60dfa2a9f15138134095eeeab1274bb409d
abeciana1 <alex.beciana@gmail.com>
2025-05-20 20:05:21 -0600

    Deconstructed props

--- components/_projects/ProjectCard/index.tsx
+++ components/_projects/ProjectCard/index.tsx
@@ -1,6 +1,14 @@
 import { type ProjectProps } from '@/types/blockTypes'
 
-const ProjectCard: React.FC<ProjectProps> = () => {
+const ProjectCard: React.FC<ProjectProps> = ({
+  slug,
+  title,
+  excerpt,
+  status,
+  tech,
+  links,
+  tags
+}) => {
   return (
     <div></div>
   )
