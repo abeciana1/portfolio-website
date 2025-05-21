997bba997c4b8cdd6d666336e9409f38b1ba57b1
abeciana1 <alex.beciana@gmail.com>
2025-05-20 19:57:22 -0600

    Added mapping for projects list

--- src/blocks/ProjectGrid/component.tsx
+++ src/blocks/ProjectGrid/component.tsx
@@ -1,10 +1,18 @@
-import { type ProjectSectionBlockProps } from '@/types/blockTypes'
+import {
+  type ProjectSectionBlockProps
+} from '@/types/blockTypes'
+import ProjectCard from '@/components/_projects/ProjectCard'
 
 const ProjectGrid: React.FC<Pick<ProjectSectionBlockProps, 'projects'>> = ({
   projects
 }) => {
+  console.log('projects list', projects)
   return (
-    <section></section>
+    <section>
+      {projects?.map((project) => {
+        return <></>
+      })}
+    </section>
   )
 }
 
