d8d6041d5d150699b70afd41a3722843f2239bca
abeciana1 <alex.beciana@gmail.com>
2025-05-22 21:35:14 -0600

    Added rendering of gradient cards

--- src/blocks/ProjectGrid/component.tsx
+++ src/blocks/ProjectGrid/component.tsx
@@ -1,31 +1,54 @@
 import {
-  type ProjectSectionBlockProps
+  type ProjectSectionBlockProps,
+  type GradientOptions
 } from '@/types/blockTypes'
 import ProjectCard from '@/components/_projects/ProjectCard'
+import Gradient from '@/components/_styled/Gradient'
 
 const ProjectGrid: React.FC<Pick<ProjectSectionBlockProps,
 'projects' | 'gradient' | 'gradientSelect'>> = ({
-  projects
+  projects,
+  gradient,
+  gradientSelect
 }) => {
+
+  const groups = projects.reduce<typeof projects[]>((acc, proj, idx) => {
+    if (idx % 4 === 0) acc.push([])
+    acc[acc.length - 1].push(proj)
+    return acc
+  }, [])
+
   return (
     <section
-      className="grid grid-cols-1 md:grid-cols-2"
+      className=""
     >
-      {projects?.map((project) => {
-        return (
-          <ProjectCard
-            key={project.slug}
-            slug={project.slug}
-            title={project.title}
-            excerpt={project.excerpt}
-            status={project.status}
-            tech={project.tech}
-            links={project.links}
-            tags={project.tags}
-            image={project.image}
-          />
-        )
-      })}
+      {groups.map((group, groupIdx) => (
+        <div
+          key={groupIdx}
+          className="relative grid grid-cols-1 md:grid-cols-2 gap-8"
+        >
+          {gradient && (
+            <div
+              className="absolute top-1/5 left-5 lg:left-1/4 rounded-full h-64 w-[45rem] blur-3xl opacity-70 z-0"
+            >
+              <Gradient variant={gradientSelect as GradientOptions} />
+            </div>
+          )}
+          {group.map((project) => (
+            <ProjectCard
+              key={project.slug}
+              slug={project.slug}
+              title={project.title}
+              excerpt={project.excerpt}
+              status={project.status}
+              tech={project.tech}
+              links={project.links}
+              tags={project.tags}
+              image={project.image}
+            />
+          ))}
+        </div>
+      ))}
     </section>
   )
 }
