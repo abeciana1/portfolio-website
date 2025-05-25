6807457fd03d9f27d8b3c67ee62d6651f67565d3
abeciana1 <alex.beciana@gmail.com>
2025-05-24 21:52:12 -0600

    Added test cases

--- components/_projects/ProjectCard/__test__/index.test.tsx
+++ components/_projects/ProjectCard/__test__/index.test.tsx
@@ -8,7 +8,7 @@ describe('ProjectCard', () => {
       <ProjectCard
         title='Project title'
         excerpt='Project excerpt'
-        slug='/projects/project-title'
+        slug='/project-title'
         image={{
           id: 1,
           alt: 'Hero Image',
@@ -28,5 +28,32 @@ describe('ProjectCard', () => {
         ]}
       />
     )
+  })
+  test('should be linked to project page', () => {
+    const card = screen.getByRole('link')
+    expect(card).toBeInTheDocument()
+    expect(card).toHaveAttribute('href', 'projects/project-title')
+  })
+  test('should render project title', () => {
+    const title = screen.getByTestId('project-title')
+    expect(title.textContent).toBe('Project title')
+    expect(title).toBeInTheDocument()
+  })
+  test('should render image', () => {
+    const image = screen.getByRole('img')
+    expect(image).toHaveAttribute('alt', 'Hero Image')
+    expect(image).toHaveAttribute('height', '100')
+    expect(image).toHaveAttribute('width', '100')
+    expect(image).toBeInTheDocument()
+  })
+  test('should render excerpt', () => {
+    const excerpt = screen.getByTestId('project-excerpt')
+    expect(excerpt.textContent).toBe('Project excerpt')
+    expect(excerpt).toBeInTheDocument()
+  })
+  test('should render tags list', () => {
+    const tagList = screen.getByTestId('tags-list')
+    expect(tagList.children.length).toBe(1)
+    expect(tagList).toBeInTheDocument()
   })
 })
