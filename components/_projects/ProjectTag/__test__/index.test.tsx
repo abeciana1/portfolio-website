eff721a427ccf3c320b3e473ccde5d155e5bc485
abeciana1 <alex.beciana@gmail.com>
2025-05-24 22:34:52 -0600

    Added test data and test cases

---
+++ components/_projects/ProjectTag/__test__/index.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import ProjectTag from '..'
+
+describe('ProjectTag', () => {
+  beforeEach(() => {
+    render(
+      <ProjectTag
+        label='Test'
+      />
+    )
+  })
+  test('should render tag', () => {
+    const tag = screen.getByRole('listitem')
+    expect(tag.textContent).toBe('Test')
+    expect(tag).toBeInTheDocument()
+  })
+})
