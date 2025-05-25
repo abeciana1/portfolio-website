3e07ddce13929eadcfce2ffd4b7b87abdc548695
abeciana1 <alex.beciana@gmail.com>
2025-05-24 22:27:11 -0600

    Added tab index

--- components/_projects/ProjectTag/index.tsx
+++ components/_projects/ProjectTag/index.tsx
@@ -9,6 +9,7 @@ const ProjectTag: React.FC<Pick<ProjectTagProps,
 }) => {
   return (
     <li
+      tabIndex={0}
       className="text-foreground bg-pillGrey rounded-lg py-1 px-2 text-center font-medium max-w-fit text-sm"
     >
       { label }
