c43a468f433b6575f074add3a0de9c34a70aaec8
abeciana1 <alex.beciana@gmail.com>
2025-05-25 22:20:28 -0600

    Added rich text content

--- src/blocks/OverviewSection/config.ts
+++ src/blocks/OverviewSection/config.ts
@@ -1,7 +1,32 @@
 import { type Block } from 'payload'
+import {
+  HTMLConverterFeature,
+  lexicalEditor,
+  lexicalHTML
+} from '@payloadcms/richtext-lexical'
 
 export const OverviewSection: Block = {
   slug: 'overview-section',
   interfaceName: 'OverviewSectionBlock',
-  fields: []
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      required: true,
+      type: 'text'
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      label: 'Rich Text Content',
+      required: true,
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          HTMLConverterFeature({}),
+        ]
+      })
+    },
+    lexicalHTML('content', { name: 'content_html' })
+  ]
 }
