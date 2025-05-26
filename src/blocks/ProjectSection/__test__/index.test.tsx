fb52bc26a88db4a9b786beaca2e6026218f8c2d9
abeciana1 <alex.beciana@gmail.com>
2025-05-25 21:54:43 -0600

    Added test cases

--- src/blocks/ProjectSection/__test__/index.test.tsx
+++ src/blocks/ProjectSection/__test__/index.test.tsx
@@ -15,4 +15,26 @@ describe('ProjectSection', () => {
       />
     )
   })
+  test('should render section', () => {
+    const section = screen.getByTestId('project-section')
+    expect(section).toBeInTheDocument()
+  })
+  test('should render pill', () => {
+    const pill = screen.getByTestId('testimonials-pill')
+    expect(pill.textContent).toBe('Projects')
+    expect(pill).toBeInTheDocument()
+  })
+  test('should render heading', () => {
+    const heading = screen.getByRole('heading', {
+      level: 1
+    })
+    expect(heading.outerHTML).toBe("<h1 class=\"z-50 text-6xl font-semibold leading-16\">Project Section</h1>")
+    expect(heading.textContent).toBe('Project Section')
+    expect(heading).toBeInTheDocument()
+  })
+  test('should render description', () => {
+    const description = screen.getByTestId('testimonials-desc')
+    expect(description.textContent).toBe('Project description')
+    expect(description).toBeInTheDocument()
+  })
 })
