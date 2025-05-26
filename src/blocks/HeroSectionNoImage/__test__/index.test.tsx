d05cfd636318c529df171820eadd71c5aa1c1c59
abeciana1 <alex.beciana@gmail.com>
2025-05-25 22:15:33 -0600

    Added test cases

--- src/blocks/HeroSectionNoImage/__test__/index.test.tsx
+++ src/blocks/HeroSectionNoImage/__test__/index.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import HeroSectionNoImage from '../component'
+import { sub } from 'date-fns'
 
 describe('HeroSectionNoImage', () => {
   beforeEach(() => {
@@ -13,5 +14,33 @@ describe('HeroSectionNoImage', () => {
         secondaryBlurb='Hero secondary blur'
       />
     )
+  })
+  test('should render hero section', () => {
+    const heroSection = screen.getByTestId('test-section')
+    expect(heroSection).toBeInTheDocument()
+  })
+  test('should render title', () => {
+    const title = screen.getByRole('heading', {
+      level: 1
+    })
+    expect(title.textContent).toBe('Test Section')
+    expect(title).toBeInTheDocument()
+  })
+  test('shoulder render subtitle', () => {
+    const subtitle = screen.getByRole('heading', {
+      level: 2
+    })
+    expect(subtitle.textContent).toBe('Subsection text')
+    expect(subtitle).toBeInTheDocument()
+  })
+  test('should render description', () => {
+    const description = screen.getByTestId('description')
+    expect(description.textContent).toBe('Hero description')
+    expect(description).toBeInTheDocument()
+  })
+  test('should render secondary blurb', () => {
+    const blurb = screen.getByTestId('secondary-blurb')
+    expect(blurb.textContent).toBe('Hero secondary blur')
+    expect(blurb).toBeInTheDocument()
   })
 })
