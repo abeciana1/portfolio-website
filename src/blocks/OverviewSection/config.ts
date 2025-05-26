28ed5cba51ad16ccd1bb94a8c8c818653f890008
abeciana1 <alex.beciana@gmail.com>
2025-05-25 22:35:14 -0600

    Created OverviewSection block config

--- src/blocks/OverviewSection/config.ts
+++ src/blocks/OverviewSection/config.ts
@@ -27,6 +27,56 @@ export const OverviewSection: Block = {
         ]
       })
     },
-    lexicalHTML('content', { name: 'content_html' })
+    lexicalHTML('content', { name: 'content_html' }),
+    {
+      name: 'role',
+      label: 'Role',
+      type: 'select',
+      hasMany: true,
+      options: [
+        { label: 'Designer', 'value': 'designer' },
+        { label: 'Frontend Developer', value: 'frontendDev' },
+        { label: 'Backend Developer', value: 'backendDEv' },
+        { label: 'Integration Specialist', value: 'integrationSpec' }
+      ],
+      required: true
+    },
+    {
+      type: 'group',
+      name: 'duration',
+      label: 'Duration',
+      fields: [
+        {
+          type: 'row',
+          fields: [
+            {
+              name: 'timeLength',
+              label: 'Time length',
+              type: 'number',
+              required: true
+            },
+            {
+              name: 'frequency',
+              label: 'Frequency',
+              type: 'select',
+              required: true,
+              options: [
+                { label: 'Years', value: 'years' },
+                { label: 'Months', value: 'months' },
+                { label: 'Weeks', value: 'weeks' }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'tags',
+      label: 'Tags',
+      type: 'relationship',
+      relationTo: 'project-tags',
+      required: true,
+      hasMany: true
+    }
   ]
 }
