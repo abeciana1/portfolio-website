46f16277b154d5be85d6604e751aca0908e95cc3
abeciana1 <alex.beciana@gmail.com>
2025-05-25 22:48:34 -0600

    Added props type and props

--- src/blocks/OverviewSection/component.tsx
+++ src/blocks/OverviewSection/component.tsx
@@ -1,8 +1,19 @@
 import { type OverviewSectionBlockProps } from '@/types/blockTypes'
+import { Heading1 } from '@/components/_styled/Heading'
 
-const OverviewSection = () => {
+const OverviewSection: React.FC<OverviewSectionBlockProps> = ({
+  title,
+  content_html,
+  role,
+  duration,
+  tags
+}) => {
   return (
-    <section></section>
+    <section
+      data-testid='overview-section'
+      id='overview-section'
+      className='relative z-50 lg:py-24 px-5 py-10'
+    ></section>
   )
 }
 
