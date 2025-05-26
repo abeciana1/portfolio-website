fe62e36ad89b997ba42f6605364ebfb3aadbb3e6
abeciana1 <alex.beciana@gmail.com>
2025-05-25 22:09:39 -0600

    Added faux data

--- src/blocks/HeroSectionNoImage/__test__/index.test.tsx
+++ src/blocks/HeroSectionNoImage/__test__/index.test.tsx
@@ -1,2 +1,17 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import HeroSectionNoImage from '../component'
+
+describe('HeroSectionNoImage', () => {
+  beforeEach(() => {
+    render(
+      <HeroSectionNoImage
+        sectionId='test-section'
+        title='Test Section'
+        subtitle='Subsection text'
+        description='Hero description'
+        secondaryBlurb='Hero secondary blur'
+      />
+    )
+  })
+})
