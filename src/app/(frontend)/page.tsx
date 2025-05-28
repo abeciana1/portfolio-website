63fe59346df25801c286b85c4419a7653477bb3a
abeciana1 <alex.beciana@gmail.com>
2025-05-27 20:30:28 -0600

    Updated slug and page data mapping

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,64 @@
-import PageTemplate, { generateMetadata } from './[...slug]/page'
+import type { Metadata } from 'next'
+import {
+  getPayload,
+  type RequiredDataFromCollectionSlug
+} from 'payload'
+import { cache } from 'react'
+import RenderBlocks from '@/src/blocks/RenderBlocks'
+import { notFound } from 'next/navigation';
+import buildConfig from '@/src/payload.config'
+import { generateMeta } from '@/utils/generateMeta'
 
-export default PageTemplate
+type Args = {
+  params: Promise<{
+    slug?: string
+  }>
+}
 
-export { generateMetadata }
+const Page = async ({ params: paramsPromise }: Args) => {
+  const { slug = 'home' } = await paramsPromise
+
+  const page: RequiredDataFromCollectionSlug<'pages'> | null = await queryPageBySlug({
+    slug,
+  })
+
+  if (!page) {
+    return notFound()
+  }
+
+  const { layout } = page
+
+  return (
+    <main className='relative'>
+      <RenderBlocks blocks={layout} />
+    </main>
+  )
+}
+
+const queryPageBySlug = cache(async ({ slug = 'home' }: { slug: string }) => {
+  const payload = await getPayload({ config: buildConfig })
+  
+  const result = await payload.find({
+    collection: 'pages',
+    limit: 1,
+    pagination: false,
+    where: {
+      slug: {
+        equals: Array.isArray(slug) ? slug[0] : slug,
+      },
+    },
+  })
+
+  return result.docs?.[0] || null
+})
+
+export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
+  const { slug = 'home' } = await paramsPromise
+  const page = await queryPageBySlug({
+    slug,
+  })
+
+  return generateMeta({ doc: page })
+}
+
+export default Page
