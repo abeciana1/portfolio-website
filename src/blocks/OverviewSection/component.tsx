896a3047ecf3b99bd07acac0ed468b8a8e6affc1
abeciana1 <alex.beciana@gmail.com>
2025-05-27 21:24:12 -0600

    Rendered meta data

--- src/blocks/OverviewSection/component.tsx
+++ src/blocks/OverviewSection/component.tsx
@@ -1,5 +1,10 @@
-import { type OverviewSectionBlockProps } from '@/types/blockTypes'
+import {
+  type OverviewSectionBlockProps,
+  type RoleSelect
+} from '@/types/blockTypes'
 import { Heading1 } from '@/components/_styled/Heading'
+import RichTextEditor from '@/src/blocks/RichTextEditor/component'
+import ProjectTag from '@/components/_projects/ProjectTag'
 
 const OverviewSection: React.FC<OverviewSectionBlockProps> = ({
   title,
@@ -8,16 +13,56 @@ const OverviewSection: React.FC<OverviewSectionBlockProps> = ({
   duration,
   tags
 }) => {
+  console.log('tags', tags)
+
+  const roles = {
+    "designer": "Designer",
+    "frontendDev": "Frontend Developer",
+    "backendDEv": "Backend Developer",
+    "integrationSpec": "Integration Specialist"
+  }
+
   return (
     <section
       data-testid='overview-section'
       id='overview-section'
-      className='relative z-50 lg:py-24 px-5 py-10'
+      className='relative z-50 lg:py-24 px-5 py-10 flex'
     >
-      <div>
+      <section className='space-y-6'>
         <Heading1 text={title} />
-      </div>
-      <div></div>
+        <RichTextEditor
+          content_html={content_html as string}
+        />
+      </section>
+      <section className=''>
+        <div>
+          <div className=''>Role</div>
+          <ul>
+            {role?.map((role, index) => {
+              return (
+                <ProjectTag
+                  key={`${role}-${index}`}
+                  label={roles[role as RoleSelect] as string}
+                />
+              )
+            })}
+          </ul>
+        </div>
+        <div>
+          <div>Duration</div>
+          <div>{`${duration.timeLength} ${duration.frequency}`}</div>
+        </div>
+        <div>
+          <div>Tags</div>
+          <ul>
+            {tags?.map((tag) => {
+              return (
+                <ProjectTag key={`${tag?.label}-${tag?.id}`} label={tag?.label} />
+              )
+            })}
+          </ul>
+        </div>
+      </section>
     </section>
   )
 }
