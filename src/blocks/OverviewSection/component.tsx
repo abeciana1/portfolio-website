29b9907b7a4aa3bd221abdfa7f20b1be0b7fec81
abeciana1 <alex.beciana@gmail.com>
2025-05-28 20:11:51 -0600

    Adjusted spacing and removed console log

--- src/blocks/OverviewSection/component.tsx
+++ src/blocks/OverviewSection/component.tsx
@@ -13,7 +13,6 @@ const OverviewSection: React.FC<OverviewSectionBlockProps> = ({
   duration,
   tags
 }) => {
-  console.log('tags', tags)
 
   const roles = {
     "designer": "Designer",
@@ -26,42 +25,46 @@ const OverviewSection: React.FC<OverviewSectionBlockProps> = ({
     <section
       data-testid='overview-section'
       id='overview-section'
-      className='relative z-50 lg:py-24 px-5 py-10 flex md:px-10'
+      className='relative z-50 lg:py-24 py-10 px-5 md:px-10 lg:px-17 mx-auto w-full'
     >
-      <section className='space-y-6'>
-        <Heading1 text={title} />
-        <RichTextEditor
-          content_html={content_html as string}
-        />
-      </section>
-      <section className=''>
-        <div>
-          <div className=''>Role</div>
-          <ul>
-            {role?.map((role, index) => {
-              return (
-                <ProjectTag
-                  key={`${role}-${index}`}
-                  label={roles[role as RoleSelect] as string}
-                />
-              )
-            })}
-          </ul>
-        </div>
-        <div>
-          <div>Duration</div>
-          <div>{`${duration.timeLength} ${duration.frequency}`}</div>
-        </div>
-        <div>
-          <div>Tags</div>
-          <ul>
-            {tags?.map((tag) => {
-              return (
-                <ProjectTag key={`${tag?.label}-${tag?.id}`} label={tag?.label} />
-              )
-            })}
-          </ul>
-        </div>
+      <Heading1 text={title} />
+      <section className='mt-10 flex justify-center'>
+        <section>
+          <div className=''>
+            <RichTextEditor
+              content_html={content_html as string}
+            />
+          </div>
+        </section>
+        <section className='w-full max-w-56'>
+          <div>
+            <div className=''>Role</div>
+            <ul>
+              {role?.map((role, index) => {
+                return (
+                  <ProjectTag
+                    key={`${role}-${index}`}
+                    label={roles[role as RoleSelect] as string}
+                  />
+                )
+              })}
+            </ul>
+          </div>
+          <div>
+            <div>Duration</div>
+            <div>{`${duration.timeLength} ${duration.frequency}`}</div>
+          </div>
+          <div>
+            <div>Tags</div>
+            <ul>
+              {tags?.map((tag) => {
+                return (
+                  <ProjectTag key={`${tag?.label}-${tag?.id}`} label={tag?.label} />
+                )
+              })}
+            </ul>
+          </div>
+        </section>
       </section>
     </section>
   )
