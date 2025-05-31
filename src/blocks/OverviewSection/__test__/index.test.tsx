db720045521748bbea5a418f5d755b993b02262f
abeciana1 <alex.beciana@gmail.com>
2025-05-31 15:47:03 -0600

    Added test render

--- src/blocks/OverviewSection/__test__/index.test.tsx
+++ src/blocks/OverviewSection/__test__/index.test.tsx
@@ -1,2 +1,26 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import OverviewSection from '../component'
+
+describe('OverviewSection', () => {
+  beforeEach(() => {
+    render(
+      <OverviewSection
+        title='Overview'
+        content_html="<div>hello testing</div>"
+        role={['designer']}
+        duration={{
+          timeLength: 3,
+          frequency: 'months'
+        }}
+        tags={[
+          {
+            id: 2,
+            label: 'test label',
+          }
+        ]}
+        greyBackground={false}
+      />
+    )
+  })
+})
