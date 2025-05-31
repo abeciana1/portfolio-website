ecaaa6ea9dd36fc0d7aa80c73399a4d878c4ba7a
abeciana1 <alex.beciana@gmail.com>
2025-05-31 15:29:08 -0600

    Added conditional styling

--- components/_projects/ProjectTag/index.tsx
+++ components/_projects/ProjectTag/index.tsx
@@ -1,16 +1,21 @@
 import {
   type ProjectTagProps
 } from '@/types/blockTypes'
+import clsx from 'clsx'
 
 const ProjectTag: React.FC<Pick<ProjectTagProps,
-  'label'
+  'label' |
+  'addBorder'
 >> = ({
-  label
+  label,
+  addBorder = false
 }) => {
   return (
     <li
       tabIndex={0}
-      className="text-foreground bg-pillGrey rounded-lg py-1 px-2 text-center font-medium max-w-fit text-sm"
+      className={clsx("text-foreground bg-pillGrey rounded-lg py-1 px-2 text-center font-medium max-w-fit text-sm", {
+        ['border-2 border-foreground dark:border-0 dark:border-transparent']: addBorder
+      })}
     >
       { label }
     </li>
