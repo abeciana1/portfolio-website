e624c642f7a8261387c92e21a5b2fd657eabec02
abeciana1 <alex.beciana@gmail.com>
2025-06-01 08:20:43 -0600

    Imported AnimatedNumber

--- components/_projects/UserResearchCard/index.tsx
+++ components/_projects/UserResearchCard/index.tsx
@@ -1,4 +1,5 @@
 import { type UserResearchCardProps } from '@/types/blockTypes'
+import { AnimatedNumber } from '@/components/motion-primitives/animated-number'
 
 const UserResearchCard: React.FC<UserResearchCardProps> = ({
   researchType,
