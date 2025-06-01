e01872739a69aeb013e6aeec46f762068b94d42b
abeciana1 <alex.beciana@gmail.com>
2025-06-01 16:43:33 -0600

    Added rendering of content

--- src/blocks/OutcomesSection/component.tsx
+++ src/blocks/OutcomesSection/component.tsx
@@ -16,7 +16,34 @@ const OutcomesSection: React.FC<OutcomesSectionBlockProps> = ({
   gradientSelect
 }) => {
   return (
-    <section></section>
+    <section
+      data-testid={sectionId as string}
+      id={sectionId as string}
+      className='relative z-50 lg:py-24 px-5 py-10 space-y-6'
+    >
+      <section className='relative flex flex-col gap-6 z-40 text-center md:max-w-2xl mx-auto'>
+        {pill &&
+          <div data-testid='outcome-pill' className='flex justify-center'>
+            <Pill text={pill as string} />
+          </div>
+        }
+        <Heading1 text={heading} />
+        <div data-testid='outcome-desc' className='text-darkGrey dark:text-pillGrey text-xl font-semibold'>
+          {description}
+        </div>
+      </section>
+      <section
+          className="relative grid grid-cols-1 md:grid-cols-2 gap-6 px-5 lg:px-20 lg:max-w-[1000px] mx-auto"
+      >
+        {gradient && (
+          <div
+            className="absolute top-1/5 left-5 lg:left-1/4 rounded-full h-64 w-[45rem] blur-3xl opacity-70 z-0"
+          >
+            <Gradient variant={gradientSelect as GradientOptions} />
+          </div>
+        )}
+      </section>
+    </section>
   )
 }
 
