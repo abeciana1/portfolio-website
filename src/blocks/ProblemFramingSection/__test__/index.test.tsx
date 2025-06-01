ca4c74cd0aef23943b334fc1a8acb4e004d7185d
abeciana1 <alex.beciana@gmail.com>
2025-05-31 20:26:20 -0600

    Added test render

--- src/blocks/ProblemFramingSection/__test__/index.test.tsx
+++ src/blocks/ProblemFramingSection/__test__/index.test.tsx
@@ -1,3 +1,17 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import ProblemFramingSection from '../component'
+
+describe('ProblemFramingSection', () => {
+  beforeEach(() => {
+    render(
+      <ProblemFramingSection
+        sectionId='problem-section'
+        pill='Problems'
+        heading='Problem Section'
+        description='Problem description'
+        problems={[]}
+      />
+    )
+  })
+})
