e9610aeebea2d68f0721776da2444a6d5146c7b3
abeciana1 <alex.beciana@gmail.com>
2025-05-31 20:48:01 -0600

    Added tests

--- src/blocks/ProblemFramingSection/__test__/index.test.tsx
+++ src/blocks/ProblemFramingSection/__test__/index.test.tsx
@@ -17,4 +17,25 @@ describe('ProblemFramingSection', () => {
       />
     )
   })
+  test('should render section', () => {
+    const section = screen.getByTestId('problem-section')
+    expect(section).toBeInTheDocument()
+  })
+  test('should render pill', () => {
+    const pill = screen.getByTestId('problem-pill')
+    expect(pill.textContent).toBe('Problems')
+    expect(pill).toBeInTheDocument()
+  })
+  test('should render heading', () => {
+    const heading = screen.getByRole('heading', {
+      level: 1
+    })
+    expect(heading.textContent).toBe('Problem Section')
+    expect(heading).toBeInTheDocument()
+  })
+  test('should render description', () => {
+    const description = screen.getByTestId('problem-desc')
+    expect(description.textContent).toBe('Problem description')
+    expect(description).toBeInTheDocument()
+  })
 })
