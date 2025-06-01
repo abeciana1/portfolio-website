438619dbe8ba175dd268c86026ef58e2cb426e93
abeciana1 <alex.beciana@gmail.com>
2025-06-01 16:40:54 -0600

    Added imports and props

--- src/blocks/OutcomesSection/component.tsx
+++ src/blocks/OutcomesSection/component.tsx
@@ -1,5 +1,20 @@
+import {
+  type OutcomesSectionBlockProps,
+  type GradientOptions
+} from '@/types/blockTypes'
+import Pill from '@/components/_styled/Pill'
+import { Heading1 } from '@/components/_styled/Heading'
+import Gradient from '@/components/_styled/Gradient'
 
-const OutcomesSection = () => {
+const OutcomesSection: React.FC<OutcomesSectionBlockProps> = ({
+  sectionId,
+  pill,
+  heading,
+  description,
+  outcomes,
+  gradient,
+  gradientSelect
+}) => {
   return (
     <section></section>
   )
