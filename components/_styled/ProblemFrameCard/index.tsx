da6c4ac94f4fe60bf201aa78d6c354f6d78a8067
abeciana1 <alex.beciana@gmail.com>
2025-05-31 21:48:12 -0600

    Adjusted styling

--- components/_styled/ProblemFrameCard/index.tsx
+++ components/_styled/ProblemFrameCard/index.tsx
@@ -21,13 +21,18 @@ const ProblemFrameCard: React.FC<ProblemFrameProps> = ({
 }) => {
   return (
     <div
-      className='relative p-6 rounded-2xl bg-background/50 dark:bg-foreground/50 h-auto max-h-[412px]'
+    className='relative p-6 rounded-2xl bg-background/50 dark:bg-foreground/50 h-auto max-h-[412px]'
     >
-      <div data-testid='frame-heading'>
-        <div>{frameHash[frame]}</div>
-        <div>{emojiHash[frame]}</div>
+      <div className='relative z-50 opacity-100 space-y-6'>
+        <div
+          data-testid='frame-heading'
+          className='flex justify-between'
+        >
+          <div>{frameHash[frame]}</div>
+          <div>{emojiHash[frame]}</div>
+        </div>
+        <div>{description}</div>
       </div>
-      <div>{description}</div>
     </div>
   )
 }
