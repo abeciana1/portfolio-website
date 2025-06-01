9715e3db69a9e113fe862020e0ee8f063db061d2
abeciana1 <alex.beciana@gmail.com>
2025-06-01 08:04:24 -0600

    Imported type and added props

--- src/blocks/UserResearchSection/component.tsx
+++ src/blocks/UserResearchSection/component.tsx
@@ -1,5 +1,17 @@
+import {
+  type UserResearchSectionBlockProps
+} from '@/types/blockTypes'
+import { Heading } from 'lucide-react'
 
-const UserResearchSection = () => {
+const UserResearchSection: React.FC<UserResearchSectionBlockProps> = ({
+  sectionId,
+  pill,
+  heading,
+  description,
+  research,
+  gradient,
+  gradientSelect
+}) => {
   return (
     <section></section>
   )
