27a927347ec7bae7cd8d74fa46ed23020917ec76
abeciana1 <alex.beciana@gmail.com>
2025-05-31 23:06:28 -0600

    Added initial config

---
+++ src/blocks/UserResearchSection/config.ts
@@ -0,0 +1,30 @@
+import { type Block } from 'payload'
+
+export const UserResearchSection: Block = {
+  slug: 'user-research',
+  interfaceName: 'UserResearchSectionBlock',
+  fields: [
+    {
+      name: 'sectionId',
+      type: 'text',
+      label: 'Section ID',
+      required: true,
+    },
+    {
+      name: 'pill',
+      type: 'text',
+      label: 'Pill',
+    },
+    {
+      name: 'heading',
+      type: 'text',
+      label: 'Heading',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'text',
+      label: 'Description'
+    },
+  ]
+}
