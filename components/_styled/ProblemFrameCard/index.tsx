6793571135aaa80e725811f0c686675e48910796
abeciana1 <alex.beciana@gmail.com>
2025-05-31 21:17:09 -0600

    Added props to rendering and created frame hash

--- components/_styled/ProblemFrameCard/index.tsx
+++ components/_styled/ProblemFrameCard/index.tsx
@@ -1,12 +1,27 @@
 import { type ProblemFrameProps } from '@/types/blockTypes'
 import React from 'react'
 
+const frameHash = {
+  'who': 'Who',
+  'where': 'Where',
+  'when': 'When',
+  'why': 'Why'
+}
+
 const ProblemFrameCard: React.FC<ProblemFrameProps> = ({
   frame,
   description
 }) => {
   return (
-    <div></div>
+    <div
+      className='relative p-6 rounded-2xl bg-background/50 dark:bg-foreground/50 h-auto max-h-[412px]'
+    >
+      <div data-testid='frame-heading'>
+        <div>{frameHash[frame]}</div>
+        <div></div>
+      </div>
+      <div>{description}</div>
+    </div>
   )
 }
 
