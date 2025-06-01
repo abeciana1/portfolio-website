4f3b5a9c8ce5ac2927bc25a6f6cc472ef00fba80
abeciana1 <alex.beciana@gmail.com>
2025-06-01 16:50:11 -0600

    Added tests

--- src/blocks/OutcomesSection/__test__/index.test.tsx
+++ src/blocks/OutcomesSection/__test__/index.test.tsx
@@ -1,2 +1,38 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import OutcomesSection from '../component'
+
+describe('OutcomesSection', () => {
+  beforeEach(() => {
+    render(
+      <OutcomesSection
+        sectionId='outcome-section'
+        pill='Outcomes'
+        heading='Outcomes'
+        description='Outcomes description'
+        outcomes={[]}
+      />
+    )
+  })
+  test('should render section', () => {
+    const section = screen.getByTestId('outcome-section')
+    expect(section).toBeInTheDocument()
+  })
+  test('should render pill', () => {
+    const pill = screen.getByTestId('outcome-pill')
+    expect(pill.textContent).toBe('Outcomes')
+    expect(pill).toBeInTheDocument()
+  })
+  test('should render heading', () => {
+    const heading = screen.getByRole('heading', {
+      level: 1
+    })
+    expect(heading.textContent).toBe('Outcomes')
+    expect(heading).toBeInTheDocument()
+  })
+  test('should render description', () => {
+    const description = screen.getByTestId('outcome-desc')
+    expect(description.textContent).toBe('Outcomes description')
+    expect(description).toBeInTheDocument()
+  })
+})
