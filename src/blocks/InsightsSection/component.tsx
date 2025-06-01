20277c744f2c61f1da5383c2155f379f4a3b7bce
abeciana1 <alex.beciana@gmail.com>
2025-06-01 15:29:33 -0600

    Added styles

--- src/blocks/InsightsSection/component.tsx
+++ src/blocks/InsightsSection/component.tsx
@@ -15,16 +15,20 @@ const InsightsSection: React.FC<InsightsSectionBlockProps> = ({
     <section
       data-testid={sectionId as string}
       id={sectionId as string}
-      className='px-5 md:px-10'
+      className='px-5 md:px-10 space-y-6 py-10 lg:py-24'
     >
       <Heading1 text={heading} />
-      <div data-testid='insight-desc' className='text-darkGrey dark:text-pillGrey text-xl font-semibold'>
+      <div data-testid='insight-desc' className='text-darkGrey dark:text-pillGrey text-xl font-semibold lg:w-1/2'>
         {description}
       </div>
-      <section>
-        <div></div>
+      <section
+        className='flex flex-col md:flex-row items-center gap-6'
+      >
+        <div
+          className='min-w-80 max-w-80 h-80 relative p-6 rounded-2xl bg-pillGrey/50 dark:bg-darkGrey/50 flex justify-center items-center text-5xl'
+        >🤔</div>
         {insights &&
-          <ul aria-label='insight-list'>
+          <ul aria-label='insight-list' className='space-y-6'>
             {insights?.map((insight, index) => {
               return (
                 <Insight
