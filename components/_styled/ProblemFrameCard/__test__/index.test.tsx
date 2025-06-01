264de7dbe509e4cb7876bc3c653c36028ef02446
abeciana1 <alex.beciana@gmail.com>
2025-05-31 23:01:57 -0600

    Added tests

--- components/_styled/ProblemFrameCard/__test__/index.test.tsx
+++ components/_styled/ProblemFrameCard/__test__/index.test.tsx
@@ -1,2 +1,24 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import ProblemFrameCard from '../'
+
+describe('ProblemFrameCard', () => {
+  beforeEach(() => {
+    render(
+      <ProblemFrameCard
+        frame='who'
+        description='who description'
+      />
+    )
+  })
+  test('should render frame heading', () => {
+    const heading = screen.getByTestId('frame-heading')
+    expect(heading.textContent).toBe('Who🤔')
+    expect(heading).toBeInTheDocument()
+  })
+  test('should render description', () => {
+    const description = screen.getByTestId('frame-desc')
+    expect(description.textContent).toBe('who description')
+    expect(description).toBeInTheDocument()
+  })
+})
