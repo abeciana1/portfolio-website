84f27b1291ca68db13de4835bb0125de60b04b0d
abeciana1 <alex.beciana@gmail.com>
2025-05-31 20:53:28 -0600

    Added initial render of comp

---
+++ components/_styled/ProblemFrameCard/index.tsx
@@ -0,0 +1,13 @@
+import { type ProblemFrameProps } from '@/types/blockTypes'
+import React from 'react'
+
+const ProblemFrameCard: React.FC<ProblemFrameProps> = ({
+  frame,
+  description
+}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default ProblemFrameCard
