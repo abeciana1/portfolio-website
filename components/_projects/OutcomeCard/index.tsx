9ce7184c6af262185a24c640d159728739b916d2
abeciana1 <alex.beciana@gmail.com>
2025-06-01 19:01:17 -0600

    Rendered content

--- components/_projects/OutcomeCard/index.tsx
+++ components/_projects/OutcomeCard/index.tsx
@@ -1,7 +1,57 @@
+import {
+  type OutcomeProps,
+  type OutcomeStatProps
+} from '@/types/blockTypes'
 
-const OutcomeCard = () => {
+const outcomeHash = {
+  'adoption': 'Adoption',
+  'retention': 'Retention',
+  'efficiency': 'Efficiency'
+}
+
+const OutcomeStat: React.FC<OutcomeStatProps> = ({
+  number,
+  label,
+  numLabel
+}) => {
+  return(
+    <div>
+      <div>{`${number}${numLabel}`}</div>
+      <div>{label}</div>
+    </div>
+  )
+}
+
+const OutcomeCard: React.FC<OutcomeProps> = ({
+  outcomeType,
+  emojis,
+  stats
+}) => {
+
   return (
-    <div></div>
+    <div
+      className='relative p-6 rounded-2xl  h-auto min-h-64 max-h-64 min-w-64 max-w-64 mx-auto'
+    >
+      <div
+        className='relative z-50 opacity-100 flex flex-col justify-between h-full grow'
+      >
+        <div
+          className='font-medium text-darkGrey dark:text-pillGrey'
+        >
+          {outcomeHash[outcomeType]}
+        </div>
+        {stats?.map((stat, index) => {
+          return (
+            <OutcomeStat
+              key={`${stat.label}-${index}`}
+              label={stat?.label}
+              number={stat?.number}
+              numLabel ={stat?.numLabel}
+            />
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
