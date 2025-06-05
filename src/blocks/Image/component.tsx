aeadba2169334259ee3f9d4334452b3511790543
abeciana1 <alex.beciana@gmail.com>
2025-06-05 16:50:47 -0600

    Added placeholder

--- src/blocks/Image/component.tsx
+++ src/blocks/Image/component.tsx
@@ -30,6 +30,7 @@ const ImageComponent: React.FC<ImageBlockProps> = ({
           height={(forcedHeight || height) as number}
           width={(forcedWidth || width) as number}
           className='relative z-50 rounded-md'
+          placeholder="blur"
         />
         {gradient &&
           <Gradient
