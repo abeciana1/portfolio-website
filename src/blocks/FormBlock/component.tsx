7c9fcaf04d32913c1ded38cb5228132601263b2f
abeciana1 <alex.beciana@gmail.com>
2025-06-14 13:45:09 -0600

    Added props

--- src/blocks/FormBlock/component.tsx
+++ src/blocks/FormBlock/component.tsx
@@ -1,5 +1,12 @@
+import { type FormBlockProps } from '@/types/blockTypes'
 
-const FormBlock = () => {
+const FormBlock: React.FC<FormBlockProps> = ({
+  formTitle,
+  destination,
+  enableIntro,
+  content_html,
+  form
+}) => {
   return (
     <></>
   )
