b44dbc1c1cba54df029206608fd696a2f7cdcf49
abeciana1 <alex.beciana@gmail.com>
2025-06-14 11:08:49 -0600

    Removed unused imports

--- src/blocks/RichTextEditor/component.tsx
+++ src/blocks/RichTextEditor/component.tsx
@@ -1,13 +1,12 @@
 import { type RichTextEditorProps } from '@/types/blockTypes'
-import DOMPurify from "isomorphic-dompurify";
+import DOMPurify from 'isomorphic-dompurify'
 
-const RichTextEditor: React.FC<RichTextEditorProps> = ({
-  content_html
-}) => {
-  const sanitizedContent = DOMPurify.sanitize(content_html);
+const RichTextEditor: React.FC<RichTextEditorProps> = (props) => {
+  const { content_html } = props
+  const sanitizedContent = DOMPurify.sanitize(content_html)
   return (
     <>
-      <div className='rich-text space-y-3' dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+      <div className="rich-text space-y-3" dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
     </>
   )
 }
