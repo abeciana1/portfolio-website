9df9a06fcba54e2a049b47e8491fb71962edf61c
abeciana1 <alex.beciana@gmail.com>
2025-07-06 11:55:35 -0600

    Imported css file

--- src/blocks/RichTextEditor/component.tsx
+++ src/blocks/RichTextEditor/component.tsx
@@ -1,12 +1,13 @@
 import { type RichTextEditorProps } from '@/types/blockTypes'
 import DOMPurify from 'isomorphic-dompurify'
+import '@/styles/html-styles.css'
 
 const RichTextEditor: React.FC<RichTextEditorProps> = (props) => {
   const { content_html } = props
   const sanitizedContent = DOMPurify.sanitize(content_html)
   return (
     <>
-      <div className="rich-text space-y-3" dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+      <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
     </>
   )
 }
