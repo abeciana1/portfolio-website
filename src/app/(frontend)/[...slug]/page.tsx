3a670b8a6facd46d4a2097a4e427e46c3e0e2030
abeciana1 <alex.beciana@gmail.com>
2025-08-05 21:15:22 -0600

    Adjusterd staleTime and removed constant new queryclient creation

--- src/app/(frontend)/[...slug]/page.tsx
+++ src/app/(frontend)/[...slug]/page.tsx
@@ -6,6 +6,7 @@ import { cache } from 'react'
 import RenderBlocks from '@/src/blocks/RenderBlocks'
 import { notFound } from 'next/navigation';
 import { generateMeta } from '@/utils/generateMeta'
+import queryClient from '@/utils/queryClient'
 import { QueryClient } from '@tanstack/react-query'
 import { payload } from '@/src/payload'
 
@@ -17,7 +18,6 @@ type Args = {
 
 const Page = async ({ params: paramsPromise }: Args) => {
   const { slug = 'home' } = await paramsPromise
-  const queryClient = new QueryClient()
 
   const page: RequiredDataFromCollectionSlug<'pages'> | null = await queryPageBySlug({
     slug,
@@ -50,14 +50,13 @@ const queryPageBySlug = cache(async ({ slug = 'home' }: { slug: string }, queryC
           }
         },
       }),
-      staleTime: process.env.NODE_ENV === 'production' ? 60 * 1000 * 10 * 3 : 60 * 1000
+      staleTime: process.env.NODE_ENV === 'production' ? 60 * 1000 : 10 * 1000
   })
   return result?.docs?.[0] || null
 })
 
 export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
   const { slug = 'home' } = await paramsPromise
-  const queryClient = new QueryClient()
   const page = await queryPageBySlug({
     slug,
   }, queryClient)
