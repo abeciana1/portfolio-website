f7953badf97c2a3e0d26afd2d816fd0e53b3ec59
abeciana1 <alex.beciana@gmail.com>
2025-08-19 16:17:05 -0600

    PORT-5 Create blog page schema

---
+++ src/collections/blog-pages.ts
@@ -0,0 +1,63 @@
+import {
+  type CollectionConfig
+} from 'payload';
+import { titleToSlug } from '@/utils/helpers'
+
+// * blocks
+import { HeroSectionNoImage } from '@/src/blocks/HeroSectionNoImage/config'
+
+// * fields
+import { NestedRoute } from '@/src/fields/nested-route'
+
+const allowableBlocks = [
+  HeroSectionNoImage
+]
+
+export const BlogPage: CollectionConfig = {
+  slug: 'blog-pages',
+  admin: {
+    useAsTitle: 'title'
+  },
+  hooks: {
+    afterChange: [
+      async ({ doc }) => {
+        if (doc.title) {
+          doc.slug = titleToSlug(doc.title);
+        }
+      }
+    ]
+  },
+  fields: [
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          label: 'Page Layout',
+          description: 'Added blocks and title to the page.',
+          fields: [
+            NestedRoute,
+            {
+              name: 'title',
+              type: 'text',
+              admin: {
+                description: 'Title for the page',
+              },
+              required: true
+            },
+            {
+              name: 'slug',
+              type: 'text',
+              label: 'Slug'
+            },
+            {
+              name: 'layout',
+              type: 'blocks',
+              label: 'Layout',
+              blocks: allowableBlocks
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
