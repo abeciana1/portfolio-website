1a7533d0ea86a868e262e5176ff501d8e4f01b76
abeciana1 <alex.beciana@gmail.com>
2025-08-27 15:50:41 -0600

    PORT-5 Created blog image block config

---
+++ src/blocks/BlogImage/config.ts
@@ -0,0 +1,21 @@
+import { type Block } from 'payload';
+
+export const BlogImage: Block = {
+  slug: 'blog-image',
+  interfaceName: 'BlogImageBlock',
+  fields: [
+    {
+      name: 'image',
+      label: 'Image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true
+    },
+    {
+      name: 'caption',
+      label: 'Caption',
+      type: 'text',
+      required: true
+    },
+  ],
+};
