5517137da705998508492fab91626e266fa3bec1
abeciana1 <alex.beciana@gmail.com>
2025-08-27 17:49:40 -0600

    PORT-5 Created initial render

---
+++ src/blocks/BlogImage/component.tsx
@@ -0,0 +1,29 @@
+import ImageComponent from '@/src/blocks/Image/component'
+import { type BlogImageProps } from '@/types/blockTypes'
+
+const BlogImage: React.FC<BlogImageProps> = ({
+  image,
+  caption
+}) => {
+
+  const {
+    width,
+    height,
+  } = image
+
+  return (
+    <figure>
+      <ImageComponent
+        image={image}
+        forcedWidth={width as number}
+        forcedHeight={height as number}
+        gradient={false}
+        gradientXFlip={false}
+        gradientYFlip={false}
+      />
+      <figcaption>{caption}</figcaption>
+    </figure>
+  )
+}
+
+export default BlogImage
