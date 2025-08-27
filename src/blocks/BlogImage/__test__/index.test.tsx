d86e9c60e7eb50e5a0dab03ee52f42aebd6f7e32
abeciana1 <alex.beciana@gmail.com>
2025-08-27 16:21:45 -0600

    PORT-5 Renamed file

---
+++ src/blocks/BlogImage/__test__/index.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+        // image={{
+        //   id: 1,
+        //   alt: 'Sample Image',
+        //   convertWebp: true,
+        //   url: 'http://www.example.com/image.jpg',
+        //   webpUrl: 'http://www.example.com/image.jpg',
+        //   updatedAt: '2025-01-23',
+        //   createdAt: '2025-01-23',
+        //   height: 100,
+        //   width: 100,
+        // }}
+
+describe('BlogImage', () => {
+  beforeEach(() => {
+    render(<></>)
+  })
+})
