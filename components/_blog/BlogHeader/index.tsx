51468468c8e009ab428b9df108e23a34939eb721
abeciana1 <alex.beciana@gmail.com>
2025-08-27 19:53:40 -0600

    PORT-5 Added initial render for blog header

--- components/_blog/BlogHeader/index.tsx
+++ components/_blog/BlogHeader/index.tsx
@@ -1,5 +1,6 @@
 import { type BlogHeaderProps } from '@/types/blockTypes'
 import Image from 'next/image'
+import { Heading1 } from '@/components/_styled/Heading'
 
 const BlogHeader: React.FC<BlogHeaderProps> = ({
   title,
@@ -9,10 +10,25 @@ const BlogHeader: React.FC<BlogHeaderProps> = ({
   tags
 }) => {
 
-
+  const {
+    webpUrl,
+    alt,
+    width = 1000,
+    height = 700
+  } = featuredImage
 
   return (
-    <section></section>
+    <section className='space-y-6'>
+      <Heading1 text={title} />
+      <div data-testid='blog-excerpt'>{excerpt}</div>
+      <Image
+        src={webpUrl}
+        alt={alt}
+        width={width as number}
+        height={height as number}
+        className='w-full h-auto object-cover object-center rounded-md'
+      />
+    </section>
   )
 }
 
