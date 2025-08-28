2b2557aeb16e10f3747419f3b178994b2f861248
abeciana1 <alex.beciana@gmail.com>
2025-08-27 18:15:41 -0600

    PORT-5 Added mock render

--- src/blocks/BlogImage/__test__/index.test.tsx
+++ src/blocks/BlogImage/__test__/index.test.tsx
@@ -1,20 +1,24 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-
-        // image={{
-        //   id: 1,
-        //   alt: 'Sample Image',
-        //   convertWebp: true,
-        //   url: 'http://www.example.com/image.jpg',
-        //   webpUrl: 'http://www.example.com/image.jpg',
-        //   updatedAt: '2025-01-23',
-        //   createdAt: '2025-01-23',
-        //   height: 100,
-        //   width: 100,
-        // }}
+import BlogImage from '../component'
 
 describe('BlogImage', () => {
   beforeEach(() => {
-    render(<></>)
+    render(
+      <BlogImage
+        image={{
+          id: 1,
+          alt: 'Sample Image',
+          convertWebp: true,
+          url: 'http://www.example.com/image.jpg',
+          webpUrl: 'http://www.example.com/image.jpg',
+          updatedAt: '2025-01-23',
+          createdAt: '2025-01-23',
+          height: 100,
+          width: 100,
+        }}
+        caption="This is a caption"
+      />
+    )
   })
 })
