15eaf35ca104e89dd2c5a7baf7cb021e37ef4750
abeciana1 <alex.beciana@gmail.com>
2025-08-27 20:15:04 -0600

    PORT-5 Created BlogHeader config

---
+++ src/blocks/BlogHeader/config.ts
@@ -0,0 +1,44 @@
+import { type Block } from 'payload';
+
+export const BlogHeader: Block = {
+  slug: 'blog-header',
+  interfaceName: 'BlogHeaderBlock',
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'excerpt',
+      label: 'Excerpt',
+      type: 'textarea',
+      required: true
+    },
+    {
+      name: 'featuredImage',
+      label: 'Featured Image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+      hasMany: false
+    },
+    {
+      name: 'category',
+      label: 'Category',
+      type: 'relationship',
+      relationTo: 'blog-categories',
+      required: true,
+      hasMany: false
+    },
+    {
+      name: 'tags',
+      label: 'Tags',
+      type: 'relationship',
+      relationTo: 'project-tags',
+      required: true,
+      hasMany: true
+    }
+  ],
+}
