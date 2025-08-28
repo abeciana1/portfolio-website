9c6349f3259c6c23dea511f612d33fc14af4ff7d
abeciana1 <alex.beciana@gmail.com>
2025-08-27 19:16:46 -0600

    PORT-5 Added initial render

---
+++ components/_blog/BlogHeader/index.tsx
@@ -0,0 +1,19 @@
+import { type BlogHeaderProps } from '@/types/blockTypes'
+import Image from 'next/image'
+
+const BlogHeader: React.FC<BlogHeaderProps> = ({
+  title,
+  excerpt,
+  featuredImage,
+  category,
+  tags
+}) => {
+
+
+
+  return (
+    <section></section>
+  )
+}
+
+export default BlogHeader
