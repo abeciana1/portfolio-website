6c1be6eb9ecc131ab3dd4cc2694791a69a2cafbc
abeciana1 <alex.beciana@gmail.com>
2025-08-27 18:45:50 -0600

    PORT-5 Created BlogHeaderProps type

--- types/blockTypes.ts
+++ types/blockTypes.ts
@@ -309,4 +309,10 @@ export type BlogImageProps = {
   caption: string;
 }
 
-export type BlogHeaderProps = {}
+export type BlogHeaderProps = {
+  title: string;
+  excerpt: string;
+  featuredImage: CMSMediaT;
+  category: ProjectTagProps;
+  tags: ProjectTagProps[];
+}
