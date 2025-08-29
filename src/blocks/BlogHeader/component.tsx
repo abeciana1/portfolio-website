66747c9d5c37c9efca0ffc0b501f363b825df187
abeciana1 <alex.beciana@gmail.com>
2025-08-28 20:16:32 -0600

    PORT-5 Added tags and category && Adjusted sizing for featured image

--- src/blocks/BlogHeader/component.tsx
+++ src/blocks/BlogHeader/component.tsx
@@ -1,6 +1,7 @@
 import { type BlogHeaderProps } from '@/types/blockTypes'
 import Image from 'next/image'
 import { Heading1 } from '@/components/_styled/Heading'
+import ProjectTag from '@/components/_projects/ProjectTag'
 
 const BlogHeader: React.FC<BlogHeaderProps> = ({
   title,
@@ -10,24 +11,38 @@ const BlogHeader: React.FC<BlogHeaderProps> = ({
   tags
 }) => {
 
+  console.log('tags', tags)
+
   const {
     webpUrl,
-    alt,
-    width = 1000,
-    height = 700
+    alt
   } = featuredImage
 
   return (
-    <section className='space-y-6'>
+    <section className='mt-12 space-y-6 relative text-center mb-6'>
+      <ul className='relative mx-auto flex justify-center'>
+        <ProjectTag label={category?.label} />
+      </ul>
       <Heading1 text={title} />
-      <div data-testid='blog-excerpt'>{excerpt}</div>
+      <div data-testid='blog-excerpt' className='italic text-lg font-medium'>{excerpt}</div>
       <Image
         src={webpUrl}
         alt={alt}
-        width={width as number}
-        height={height as number}
-        className='w-full h-auto object-cover object-center rounded-md'
+        width={700}
+        height={1400}
+        className='mx-auto object-cover object-center rounded-md'
       />
+      <ul
+        tabIndex={0}
+        data-testid='tags-list'
+        className='flex gap-2 flex-wrap'
+      >
+        {tags?.map((tag) => {
+          return (
+            <ProjectTag key={tag?.id} label={tag?.label} />
+          )
+        })}
+      </ul>
     </section>
   )
 }
