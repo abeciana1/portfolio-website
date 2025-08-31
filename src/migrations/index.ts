b1e67605b3d7ed61f697eb4ad9240fe704da8573
abeciana1 <alex.beciana@gmail.com>
2025-08-31 17:51:20 -0600

    PORT-5 Added mirations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250831_210036 from './20250831_210036';
 import * as migration_20250831_224704 from './20250831_224704';
+import * as migration_20250831_235000 from './20250831_235000';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250831_224704.up,
     down: migration_20250831_224704.down,
-    name: '20250831_224704'
+    name: '20250831_224704',
+  },
+  {
+    up: migration_20250831_235000.up,
+    down: migration_20250831_235000.down,
+    name: '20250831_235000'
   },
 ];
