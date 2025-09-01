0305f59f080f9e1bd423b67d7c9519c79ef15b5a
abeciana1 <alex.beciana@gmail.com>
2025-08-31 20:35:48 -0600

    PORT-5 Removed import issue

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,13 +1,8 @@
-import * as migration_20250831_210036 from './20250831_210036';
+
 import * as migration_20250831_224704 from './20250831_224704';
 import * as migration_20250831_235000 from './20250831_235000';
 
 export const migrations = [
-  {
-    up: migration_20250831_210036.up,
-    down: migration_20250831_210036.down,
-    name: '20250831_210036',
-  },
   {
     up: migration_20250831_224704.up,
     down: migration_20250831_224704.down,
