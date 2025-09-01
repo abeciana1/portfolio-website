d63343bfdd609e2d7e94e30a116a016e6bb11d60
abeciana1 <alex.beciana@gmail.com>
2025-09-01 17:39:30 -0600

    PORT-5 BlogPostSectionBlock config created

---
+++ src/blocks/BlogPostSection/config.ts
@@ -0,0 +1,47 @@
+import { type Block } from 'payload'
+
+export const BlogPostSectionBlock: Block = {
+  slug: 'blog-post-section',
+  interfaceName: 'BlogPostSectionBlock',
+  fields: [
+    {
+      name: 'postSelection',
+      type: 'select',
+      hasMany: false,
+      options: [
+        { label : 'Latest', value: 'latest' },
+        { label : 'Custom', value: 'custom' },
+        { label : 'All', value: 'all' },
+        { label : 'By Category', value: 'byCategory' }
+      ]
+    },
+    // * Set post limit for rendering
+    {
+      name: 'postLimit',
+      label: 'Limit',
+      type: 'number'
+    },
+    // * For by category post selection
+    {
+      name: 'categoryFilter',
+      label: 'Category filter',
+      type: 'relationship',
+      relationTo: 'blog-categories',
+      hasMany: false,
+      admin: {
+        condition: (_, siblingData) => siblingData.postSelection === 'byCategory'
+      }
+    },
+    // * For custom post selection
+    {
+      name: 'posts',
+      label: 'Posts',
+      type: 'relationship',
+      relationTo: 'blog-pages',
+      hasMany: true,
+      admin: {
+        condition: (_, siblingData) => siblingData.postSelection === 'custom'
+      }
+    },
+  ]
+}
