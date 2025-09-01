57fad34bcf2a82f4aacc630cbb53e35c121e5861
abeciana1 <alex.beciana@gmail.com>
2025-09-01 16:01:36 -0600

    PORT-5 Added copy clipboard functionality and sidebar

--- src/blocks/BlogBody/component.tsx
+++ src/blocks/BlogBody/component.tsx
@@ -1,16 +1,45 @@
+'use client'
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { type BlogBodyBlockProps } from '@/types/blockTypes'
-import RenderBlogBlocks from '@/src/blocks/RenderBlogBlocks'
+import dynamic from 'next/dynamic'
+const RenderBlogBlocks = dynamic(() => import('@/src/blocks/RenderBlogBlocks'), { ssr: true })
+import { useCopyToClipboard } from "@uidotdev/usehooks"
+import { usePathname } from 'next/navigation'
+import ShareBtn from '@/components/_styled/ShareBtn'
+import { Copy } from 'lucide-react';
 
 const BlogBodyBlock: React.FC<BlogBodyBlockProps> = ({
   embedBlocks
 }) => {
+  const [ copiedText, copyToClipboard ] = useCopyToClipboard();
+  const pathname = usePathname()
+
+  const hasCopiedText = Boolean(copiedText)
+
+  console.log('pathname', pathname)
+
+  const copyHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    copyToClipboard('copy')
+  }
+
   return (
-    <section
-      className='relative max-w-3xl mx-auto text-justify'
-    >
-      <RenderBlogBlocks blocks={[...embedBlocks] as any} />
-    </section>
+    <>
+      <div className='sticky top-10 left-0'>
+        <div className='space-y-6'>
+          <ShareBtn
+            onClick={copyHandler}
+            text={hasCopiedText ? 'Copied!' : 'Copy link'}
+            icon={Copy}
+          />
+        </div>
+      </div>
+      <section
+        className='relative max-w-3xl mx-auto text-justify'
+      >
+        <RenderBlogBlocks blocks={[...embedBlocks] as any} />
+      </section>
+    </>
   )
 }
 
