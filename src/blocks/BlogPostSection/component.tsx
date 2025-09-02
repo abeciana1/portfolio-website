a5aecb4977b9c0546cdd2655e1e82802fe73b9e8
abeciana1 <alex.beciana@gmail.com>
2025-09-02 11:53:52 -0600

    PORT-5 Created switch-case rendering for blog section

--- src/blocks/BlogPostSection/component.tsx
+++ src/blocks/BlogPostSection/component.tsx
@@ -1,8 +1,100 @@
+import { payload } from '@/src/payload'
+import { type BlogPostSectionBlockProps } from '@/types/blockTypes'
 
-const BlogPostSection = () => {
-  return (
-    <section></section>
-  )
+const BlogPostSection: React.FC<BlogPostSectionBlockProps> = async ({
+  postSelection,
+  postLimit,
+  categoryFilter,
+  posts
+}) => {
+  switch (postSelection) {
+    case "latest":
+      const latestBlogPosts = await payload.find({
+        collection: 'blog-pages',
+        limit: postLimit ?? 3,
+        sort: '-publishedDate',
+        where: {
+          slug: {
+            not_equals: 'home'
+          }
+        },
+        select: {
+          title: true,
+          slug: true,
+          publishedDate: true,
+          category: true,
+          tags: true,
+          meta: {
+            image: true
+          }
+        }
+      })
+      console.log('latest blogPosts', latestBlogPosts)
+      return (
+        <section></section>
+      )
+    case "byCategory":
+      const categoryBlogPosts = await payload.find({
+        collection: 'blog-pages',
+        limit: postLimit ?? 3,
+        where: {
+          category: {
+            equals: categoryFilter,
+          },
+          slug: {
+            not_equals: 'home'
+          }
+        },
+        sort: '-publishedDate',
+        select: {
+          title: true,
+          slug: true,
+          publishedDate: true,
+          category: true,
+          tags: true,
+          meta: {
+            image: true
+          }
+        }
+      })
+      console.log('category blogPosts', categoryBlogPosts)
+      return (
+        <section></section>
+      )
+    case "all":
+      const allBlogPosts = await payload.find({
+        collection: 'blog-pages',
+        limit: postLimit ?? 3,
+        sort: '-publishedDate',
+        where: {
+          slug: {
+            not_equals: 'home'
+          }
+        },
+        select: {
+          title: true,
+          slug: true,
+          publishedDate: true,
+          category: true,
+          tags: true,
+          meta: {
+            image: true
+          }
+        }
+      })
+      console.log('all blogPosts', allBlogPosts)
+      return (
+        <section></section>
+      )
+    case "custom":
+      return (
+        <section></section>
+    )
+    default:
+      return (
+        <section></section>
+      )
+  }
 }
 
 export default BlogPostSection
