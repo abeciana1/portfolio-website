dc667bfde0734365b71458752e75537c2cc2d8e1
abeciana1 <alex.beciana@gmail.com>
2025-09-03 11:32:12 -0600

    PORT-5 Added conditional styling for dark mode verison of component

--- components/_projects/ProjectTag/index.tsx
+++ components/_projects/ProjectTag/index.tsx
@@ -5,16 +5,19 @@ import clsx from 'clsx'
 
 const ProjectTag: React.FC<Pick<ProjectTagProps,
   'label' |
-  'addBorder'
+  'addBorder' | 
+  'enableDarkMode'
 >> = ({
   label,
-  addBorder = false
+  addBorder = false,
+  enableDarkMode = false
 }) => {
   return (
     <li
       tabIndex={0}
       className={clsx("text-foreground bg-pillGrey rounded-lg py-1 px-2 text-center font-medium max-w-fit text-sm", {
-        ['border-2 border-foreground dark:border-0 dark:border-transparent']: addBorder
+        ['border-2 border-foreground']: addBorder,
+        ['dark:border-0 dark:border-transparent']: enableDarkMode
       })}
     >
       { label }
