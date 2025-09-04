37308c7f70c04cd00645aa1e1c2a7dc1df713649
abeciana1 <alex.beciana@gmail.com>
2025-09-04 10:43:15 -0600

    PORT-5 Created migrations

---
+++ src/migrations/20250904_164227_create_blogs.ts
@@ -0,0 +1,102 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TYPE "public"."enum_pages_blocks_blog_post_section_post_selection" AS ENUM('latest', 'custom', 'all', 'byCategory');
+  CREATE TYPE "public"."enum_blog_pages_blocks_blog_post_section_post_selection" AS ENUM('latest', 'custom', 'all', 'byCategory');
+  CREATE TABLE "users_sessions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"created_at" timestamp(3) with time zone,
+  	"expires_at" timestamp(3) with time zone NOT NULL
+  );
+  
+  CREATE TABLE "pages_blocks_blog_post_section" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"post_selection" "enum_pages_blocks_blog_post_section_post_selection" NOT NULL,
+  	"post_limit" numeric,
+  	"category_filter_id" integer,
+  	"block_name" varchar
+  );
+  
+  CREATE TABLE "blog_pages_blocks_blog_post_section" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"post_selection" "enum_blog_pages_blocks_blog_post_section_post_selection" NOT NULL,
+  	"post_limit" numeric,
+  	"category_filter_id" integer,
+  	"block_name" varchar
+  );
+  
+  ALTER TABLE "test" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "test" CASCADE;
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_test_fk";
+  
+  DROP INDEX "payload_locked_documents_rels_test_id_idx";
+  ALTER TABLE "blog_pages" ALTER COLUMN "is_blog_post" SET DEFAULT false;
+  ALTER TABLE "pages_rels" ADD COLUMN "blog_pages_id" integer;
+  ALTER TABLE "blog_pages" ADD COLUMN "teaser_content" jsonb;
+  ALTER TABLE "blog_pages" ADD COLUMN "content_html" varchar;
+  ALTER TABLE "blog_pages_rels" ADD COLUMN "blog_pages_id" integer;
+  ALTER TABLE "users_sessions" ADD CONSTRAINT "users_sessions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "pages_blocks_blog_post_section" ADD CONSTRAINT "pages_blocks_blog_post_section_category_filter_id_blog_categories_id_fk" FOREIGN KEY ("category_filter_id") REFERENCES "public"."blog_categories"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "pages_blocks_blog_post_section" ADD CONSTRAINT "pages_blocks_blog_post_section_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."pages"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "blog_pages_blocks_blog_post_section" ADD CONSTRAINT "blog_pages_blocks_blog_post_section_category_filter_id_blog_categories_id_fk" FOREIGN KEY ("category_filter_id") REFERENCES "public"."blog_categories"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "blog_pages_blocks_blog_post_section" ADD CONSTRAINT "blog_pages_blocks_blog_post_section_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."blog_pages"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "users_sessions_order_idx" ON "users_sessions" USING btree ("_order");
+  CREATE INDEX "users_sessions_parent_id_idx" ON "users_sessions" USING btree ("_parent_id");
+  CREATE INDEX "pages_blocks_blog_post_section_order_idx" ON "pages_blocks_blog_post_section" USING btree ("_order");
+  CREATE INDEX "pages_blocks_blog_post_section_parent_id_idx" ON "pages_blocks_blog_post_section" USING btree ("_parent_id");
+  CREATE INDEX "pages_blocks_blog_post_section_path_idx" ON "pages_blocks_blog_post_section" USING btree ("_path");
+  CREATE INDEX "pages_blocks_blog_post_section_category_filter_idx" ON "pages_blocks_blog_post_section" USING btree ("category_filter_id");
+  CREATE INDEX "blog_pages_blocks_blog_post_section_order_idx" ON "blog_pages_blocks_blog_post_section" USING btree ("_order");
+  CREATE INDEX "blog_pages_blocks_blog_post_section_parent_id_idx" ON "blog_pages_blocks_blog_post_section" USING btree ("_parent_id");
+  CREATE INDEX "blog_pages_blocks_blog_post_section_path_idx" ON "blog_pages_blocks_blog_post_section" USING btree ("_path");
+  CREATE INDEX "blog_pages_blocks_blog_post_section_category_filter_idx" ON "blog_pages_blocks_blog_post_section" USING btree ("category_filter_id");
+  ALTER TABLE "pages_rels" ADD CONSTRAINT "pages_rels_blog_pages_fk" FOREIGN KEY ("blog_pages_id") REFERENCES "public"."blog_pages"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "blog_pages_rels" ADD CONSTRAINT "blog_pages_rels_blog_pages_fk" FOREIGN KEY ("blog_pages_id") REFERENCES "public"."blog_pages"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "pages_rels_blog_pages_id_idx" ON "pages_rels" USING btree ("blog_pages_id");
+  CREATE INDEX "blog_pages_rels_blog_pages_id_idx" ON "blog_pages_rels" USING btree ("blog_pages_id");
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "test_id";`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TABLE "test" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  ALTER TABLE "users_sessions" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "pages_blocks_blog_post_section" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "blog_pages_blocks_blog_post_section" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "users_sessions" CASCADE;
+  DROP TABLE "pages_blocks_blog_post_section" CASCADE;
+  DROP TABLE "blog_pages_blocks_blog_post_section" CASCADE;
+  ALTER TABLE "pages_rels" DROP CONSTRAINT "pages_rels_blog_pages_fk";
+  
+  ALTER TABLE "blog_pages_rels" DROP CONSTRAINT "blog_pages_rels_blog_pages_fk";
+  
+  DROP INDEX "pages_rels_blog_pages_id_idx";
+  DROP INDEX "blog_pages_rels_blog_pages_id_idx";
+  ALTER TABLE "blog_pages" ALTER COLUMN "is_blog_post" DROP DEFAULT;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "test_id" integer;
+  CREATE INDEX "test_updated_at_idx" ON "test" USING btree ("updated_at");
+  CREATE INDEX "test_created_at_idx" ON "test" USING btree ("created_at");
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_test_fk" FOREIGN KEY ("test_id") REFERENCES "public"."test"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "payload_locked_documents_rels_test_id_idx" ON "payload_locked_documents_rels" USING btree ("test_id");
+  ALTER TABLE "pages_rels" DROP COLUMN "blog_pages_id";
+  ALTER TABLE "blog_pages" DROP COLUMN "teaser_content";
+  ALTER TABLE "blog_pages" DROP COLUMN "content_html";
+  ALTER TABLE "blog_pages_rels" DROP COLUMN "blog_pages_id";
+  DROP TYPE "public"."enum_pages_blocks_blog_post_section_post_selection";
+  DROP TYPE "public"."enum_blog_pages_blocks_blog_post_section_post_selection";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,6 @@
-
 import * as migration_20250831_224704 from './20250831_224704';
 import * as migration_20250831_235000 from './20250831_235000';
+import * as migration_20250904_164227_create_blogs from './20250904_164227_create_blogs';
 
 export const migrations = [
   {
@@ -11,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250831_235000.up,
     down: migration_20250831_235000.down,
-    name: '20250831_235000'
+    name: '20250831_235000',
+  },
+  {
+    up: migration_20250904_164227_create_blogs.up,
+    down: migration_20250904_164227_create_blogs.down,
+    name: '20250904_164227_create_blogs'
   },
 ];
