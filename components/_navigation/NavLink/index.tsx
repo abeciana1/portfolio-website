f5dce1098a4368be3165bdf169e15fccf7459725
abeciana1 <alex.beciana@gmail.com>
2025-09-05 16:07:42 -0600

    PORT-6 Added click tracking on navigation

--- components/_navigation/NavLink/index.tsx
+++ components/_navigation/NavLink/index.tsx
@@ -4,6 +4,8 @@ import { type NavLink as NavLinkI } from '@/src/payload-types'
 import Link from 'next/link'
 import clsx from 'clsx'
 import { sendGAEvent } from '@next/third-parties/google'
+import { usePostHog } from 'posthog-js/react'
+import { useCallback } from 'react'
 
 export type NavLinkType = Pick<NavLinkI, 'link' | 'label'> & {
   onClick?: () => void;
@@ -12,14 +14,22 @@ export type NavLinkType = Pick<NavLinkI, 'link' | 'label'> & {
 const NavLink: React.FC<NavLinkType> = ({ label, link, onClick }) => {
   const pathname = usePathname()
   const isActive = pathname === link
+
+  const posthog = usePostHog()
+  const userId = posthog.get_distinct_id()
+
+  const clickTrackHandler = useCallback(() => {
+    sendGAEvent('event', 'navLinkClick', { value: { page: label }})
+    posthog.capture('nav_link_clicked', {
+      destinationSlug: link,
+      eventLocation: 'navigation',
+      userId: userId,
+      label: label
+    })
+  }, [label, link, userId, posthog])
+
   return (
-    <li onClick={() => {
-      sendGAEvent('event', 'navLinkClick', { value: { page: label }})
-      if (onClick) {
-        onClick()
-      }
-      
-    }}>
+    <li onClick={clickTrackHandler}>
       <Link
         prefetch
         aria-label={label}
