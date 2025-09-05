df516e2292c46a9ba42668af249450e93a33ff5d
abeciana1 <alex.beciana@gmail.com>
2025-09-05 13:08:22 -0600

    PORT-6 Removed comments

--- components/providers/posthog.tsx
+++ components/providers/posthog.tsx
@@ -10,8 +10,8 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
-      capture_pageview: process.env.NODE_ENV === 'production' // Disable automatic pageview capture, as we capture manually
+      person_profiles: 'always',
+      capture_pageview: process.env.NODE_ENV === 'production'
     })
   }, [])
 
