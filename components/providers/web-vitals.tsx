f12f20f540971f488b8b5eae6a2b9dfaccb2c2a0
abeciana1 <alex.beciana@gmail.com>
2025-09-05 13:21:06 -0600

    PORT-6 Added posthog vitals capture

--- components/providers/web-vitals.tsx
+++ components/providers/web-vitals.tsx
@@ -1,11 +1,15 @@
 'use client'
-
 import { useReportWebVitals } from 'next/web-vitals'
-import React from 'react'
+import { usePostHog } from 'posthog-js/react'
 
 export function WebVitals(): React.ReactElement | null {
+  const posthog = usePostHog()
   useReportWebVitals((metric) => {
-    console.log('Web Vitals', metric)
+    if (process.env.NODE_ENV === 'development') {
+      console.log('Web Vitals', metric)
+    } else if (process.env.NODE_ENV === 'production' && posthog) {
+      posthog.capture(`Web Vitals — ${metric.name} — ${metric.rating}`, metric)
+    }
   })
 
   return null
