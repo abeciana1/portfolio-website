bcd3858d494f9668b951fd0b94d8517d3b7e6c70
abeciana1 <alex.beciana@gmail.com>
2025-09-05 20:34:54 -0600

    PORT-18 Formatted w/ prettier

--- components/_navigation/ScrollToTop/index.tsx
+++ components/_navigation/ScrollToTop/index.tsx
@@ -1,17 +1,16 @@
 'use client'
-import { SquareChevronUp } from 'lucide-react';
+import { SquareChevronUp } from 'lucide-react'
 
 const ScrollToTop = () => {
-
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
 
   return (
     <button
-      data-testid='scroll-to-top'
-      aria-label='Scroll to the top of the page'
-      className='transition delay-50 duration-100 ease-in-out hover:bg-zinc-200 rounded-md h-10 w-10 flex justify-center items-center text-foreground'
+      data-testid="scroll-to-top"
+      aria-label="Scroll to the top of the page"
+      className="transition delay-50 duration-100 ease-in-out hover:bg-zinc-200 rounded-md h-10 w-10 flex justify-center items-center text-foreground"
       onClick={scrollToTop}
     >
       <SquareChevronUp size={24} />
