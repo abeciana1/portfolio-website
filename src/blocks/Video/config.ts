e6abf415d06936f13c785f3e3394b90c7ebd8ae5
abeciana1 <alex.beciana@gmail.com>
2025-09-24 19:29:34 -0600

    PORT-28 Created video block config

---
+++ src/blocks/Video/config.ts
@@ -0,0 +1,79 @@
+import { type Block } from 'payload';
+
+export const Video: Block = {
+  slug: 'video-block',
+  interfaceName: 'VideoBlock',
+  fields: [
+    {
+      name: 'video',
+      label: 'Video',
+      type: 'upload',
+      relationTo: 'video',
+      required: true,
+    },
+    {
+      name: 'forcedWidth',
+      type: 'number',
+      label: 'Force image width (in pixels)',
+      admin: {
+        description: 'Set a specific width for the image, overriding the aspect ratio'
+      },
+      required: true,
+      defaultValue: 0
+    },
+    {
+      name: 'forcedHeight',
+      type: 'number',
+      label: 'Force image height (in pixels)',
+      admin: {
+        description: 'Set a specific height for the image, overriding the aspect ratio'
+      },
+      required: true,
+      defaultValue: 0
+    },
+    {
+      name: 'gradient',
+      label: 'Add gradient to image',
+      type: 'checkbox',
+      admin: {
+        description: 'Add a gradient aura to the image'
+      },
+      defaultValue: false,
+    },
+    {
+      name: 'gradientYFlip',
+      label: 'Flip gradient along X-axis',
+      type: 'checkbox',
+      defaultValue: false,
+      admin: {
+        condition: (data, siblingData) => {
+          if (siblingData?.gradient) {
+            return true
+          } else {
+            return false
+          }
+        }
+      }
+    },
+    {
+      name: 'gradientSelect',
+      label: 'Gradient variant select',
+      type: 'select',
+      options: [
+        { label: 'Variant 1', value: 'Variant1' },
+        { label: 'Variant 2', value: 'Variant2' },
+        { label: 'Variant 3', value: 'Variant3' },
+        { label: 'Variant 4', value: 'Variant4' }
+      ],
+      admin: {
+        condition: (data, siblingData) => {
+          if (siblingData?.gradient) {
+            return true
+          } else {
+            return false
+          }
+        }
+      }
+    },
+  ],
+}
